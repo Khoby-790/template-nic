b4e6eac82f3c76409706f83661409d9bd1fc21f7
Khoby-790 <ebaidoo79@gmail.com>
2021-02-27 21:21:02 +0000

    --- Auto-Git Commit ---

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -18,20 +18,7 @@ const AppLayout = (props: Props) => {
                     {/* <!-- Narrow sidebar--> */}
                     <Sidebar />
                     {/* <!-- Main area --> */}
-                    <main className="min-w-0 flex-1 border-t border-gray-200 lg:flex">
-                        {/* <!-- Primary column --> */}
-                        <section aria-labelledby="primary-heading" className="min-w-0 flex-1 h-full flex flex-col overflow-hidden lg:order-last">
-                            <h1 id="primary-heading" className="sr-only">Home</h1>
-                            {/* <!-- Your content --> */}
-                        </section>
-
-                        {/* <!-- Secondary column (hidden on smaller screens) --> */}
-                        <aside className="hidden lg:block lg:flex-shrink-0 lg:order-first">
-                            <div className="h-full relative flex flex-col w-96 border-r border-gray-200 bg-gray-100">
-                                {/* <!-- Your content --> */}
-                            </div>
-                        </aside>
-                    </main>
+                    
                 </div>
             </div>
 
