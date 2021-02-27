7a965207aefd139149aa2b6fed5af6cc27c46daa
Khoby-790 <ebaidoo79@gmail.com>
2021-02-27 23:55:53 +0000

    --- Auto-Git Commit ---

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,67 @@
-import React from 'react'
+import React, { useRef, Fragment, ReactNode } from "react";
+import PropTypes from "prop-types";
+import Transition from "./Transition";
+import { useOutsideClick } from "../hooks";
+
 
 interface Props {
-
+    show: boolean,
+    setShow: (prev) => void,
+    size?: number,
+    height?: number,
+    children: ReactNode,
+    canClose?: boolean,
 }
 
-const Modal = (props: Props) => {
+const Modal = ({ show, setShow, size = 35, children, canClose, height }: Props) => {
+    const ref = useRef(null);
+    useOutsideClick(ref, () => {
+        if (canClose) setShow(false);
+    });
     return (
-        <div>
+        <Fragment>
+            <Transition show={show}>
+                <div class="fixed z-20 bottom-0 inset-x-0 px-4 pb-6 sm:inset-0 sm:p-0 sm:flex sm:items-center sm:justify-center">
+                    <Transition
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-out duration-300"
+                        leaveFrom="translate-x-0"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed  inset-0 transition-opacity">
+                            <div className="absolute inset-0 bg-black opacity-50" />
+                        </div>
+                    </Transition>
 
-        </div>
+                    <Transition
+                        enter="transform transition ease-in-out duration-500 sm:duration-700"
+                        enterFrom="translate-x-full"
+                        enterTo="opacity-100 translate-x-0 sm:scale-100"
+                        leave="transform transition ease-in-out duration-500 sm:duration-700"
+                        leaveFrom="opacity-100 translate-x-0 sm:scale-100"
+                        leaveTo="-translate-x-full"
+                    >
+                        <div className="fixed z-20 bottom-0 inset-x-0   sm:inset-0 sm:p-0 sm:flex sm:items-center sm:justify-center">
+                            <div
+                                ref={ref}
+                                style={{ width: `${size}vw`, height: `${height}vh` }}
+                                className="bg-white  rounded-none  overflow-hidden shadow-xl transform transition-all"
+                                role="dialog"
+                                aria-modal="true"
+                                aria-labelledby="modal-headline"
+                            >
+                                {children}
+                            </div>
+                        </div>
+                    </Transition>
+                </div>
+            </Transition>
+        </Fragment>
     )
 }
 
 export default Modal
+
+
