4bfb8a45bc4438ecce3a2c4a98fdd0bae237a5cc
Khoby-790 <ebaidoo79@gmail.com>
2021-02-27 22:06:52 +0000

    --- Auto-Git Commit ---

---
+++ src/store/actionTypes.ts
@@ -0,0 +1,2 @@
+export const OPEN_SUB_MENU = "OPEN_SUB_MENU";
+export const CLOSE_SUB_MENU = "CLOSE_SUB_MENU";
--- src/type.d.ts
+++ src/type.d.ts
@@ -2,3 +2,5 @@ type SidebarAction = {
   type: string;
   route?: any;
 };
+
+type SidebarDispatchType = (args: SidebarAction) => SidebarAction;
