0f3723ef3e8dc8b80a4c97a1adead5cf3ae45f74
Khoby-790 <ebaidoo79@gmail.com>
2021-02-27 22:51:37 +0000

    --- Auto-Git Commit ---

--- src/layouts/SidebarSubMenu.tsx
+++ src/layouts/SidebarSubMenu.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef } from 'react'
 import { useSelector } from 'react-redux'
 
 interface Props {
@@ -6,10 +6,11 @@ interface Props {
 }
 
 const SidebarSubMenu = (props: Props) => {
+    const menuRef = useRef(null);
     const isOpen = useSelector<SidebarState>(state => state.sidebarOpen)
     return !isOpen ? null : (
         <Fragment>
-            <aside className="hidden lg:block lg:flex-shrink-0 lg:order-first">
+            <aside ref={menuRef} className="hidden lg:block lg:flex-shrink-0 lg:order-first">
                 <div className="h-full relative flex flex-col w-64 border-r border-gray-200 bg-gray-200">
                     {/* <!-- Your content --> */}
                 </div>
