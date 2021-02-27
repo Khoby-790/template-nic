70aee08f73cc55a5ac7812938a424368b08f92cc
Khoby-790 <ebaidoo79@gmail.com>
2021-02-27 23:36:15 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -100,7 +100,7 @@ const Dashboard = (props: Props) => {
                                                 Name
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Title
+                                                Ceeding
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Status
