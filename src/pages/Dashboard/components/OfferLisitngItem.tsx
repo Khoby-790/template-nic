ec39913b580015c3e7745a7482247386c7563d8e
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 09:03:31 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/OfferLisitngItem.tsx
+++ src/pages/Dashboard/components/OfferLisitngItem.tsx
@@ -1,13 +1,16 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import { Modal, Transition } from '../../../components'
+import { useOutsideClick } from '../../../hooks';
 
 interface Props {
     index: number
 }
 
 const OfferLisitngItem = (props: Props) => {
+    const ref = useRef(null)
     const [showOffer, setShowOffer] = useState(false);
     const [showMenu, setShowMenu] = useState(false);
+    useOutsideClick(ref, () => setShowMenu(false))
     return (
         <Fragment>
             <tr>
@@ -72,7 +75,7 @@ const OfferLisitngItem = (props: Props) => {
                                 leaveFrom="transform opacity-100 scale-100"
                                 leaveTo="transform opacity-0 scale-95"
                             >
-                                <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                                <div ref={ref} className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                                     <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
                                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
