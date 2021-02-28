5f12c889a7b5c6a69ee95ad2821cf0d65d235443
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 15:17:01 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/ReportQueryForm.tsx
+++ src/pages/Reports/ReportQueryForm.tsx
@@ -8,8 +8,11 @@ const ReportQueryForm = (props: Props) => {
     return (
         <Fragment>
             <div className="bg-white mx-5 py-3 mt-4 shadow-md rounded-md">
-                <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2">
-                    <div className="flex flex-col"></div>
+                <div className="grid mx-5 grid-cols-2 sm:grid-cols-1 md:grid-cols-2">
+                    <div className="flex flex-col">
+                        <label htmlFor="">Reinsurer</label>
+                        <input type="text" className="border border-gray-300 outline-none focus:outline-none"/>
+                    </div>
                 </div>
             </div>
         </Fragment>
