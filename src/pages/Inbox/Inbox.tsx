81f173394449379b35cad21cf69a0185c219cbfd
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 19:59:37 +0000

    --- Auto-Git Commit ---

--- src/pages/Inbox/Inbox.tsx
+++ src/pages/Inbox/Inbox.tsx
@@ -100,7 +100,7 @@ const Inbox = (props: Props) => {
                                                     leaveFrom="transform opacity-100 scale-100"
                                                     leaveTo="transform opacity-0 scale-95"
                                                 >
-                                                    <div className="origin-top-right absolute right-0 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-3">
+                                                    <div ref={detailsRef} className="origin-top-right absolute right-0 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-3">
                                                         <div className="py-1">
                                                             <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900" role="menuitem">
                                                                 Note
