8534bd952bda5be1c675e676d9a64efde8f6461b
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 15:21:23 +0000

    --- Auto-Git Commit ---

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,17 @@
 import React, { Fragment } from 'react'
 
 interface Props {
-
+    label?: string
 }
 
 
 type InputHtmlProps = HTMLInputElement
 
-const Input = ({ name, value, id }: Props & InputHtmlProps) => {
+const Input = ({ label }: Props & InputHtmlProps) => {
     return (
         <Fragment>
             <div className="flex flex-col">
-                <label htmlFor="">Reinsurer</label>
+                <label htmlFor="">{label}</label>
                 <input type="text" className="border border-gray-300 h-11 px-3 outline-none focus:outline-none" />
             </div>
         </Fragment>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Transition } from "./Transition";
 export { default as Modal } from "./Modal";
+export { default as Input } from "./Input";
