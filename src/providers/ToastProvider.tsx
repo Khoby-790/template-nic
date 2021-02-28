7d0204a011ba295e80455e282ee44f7bb5aff62a
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 00:25:21 +0000

    --- Auto-Git Commit ---

--- src/providers/ToastProvider.tsx
+++ src/providers/ToastProvider.tsx
@@ -28,6 +28,7 @@ const ToastProvider: React.FC = ({ children }) => {
     }) => {
         setMessage(message);
         setDescription(description)
+        setShowNotif(true)
     }
 
     return (
@@ -45,7 +46,15 @@ const ToastProvider: React.FC = ({ children }) => {
       From: "opacity-100"
       To: "opacity-0"
   --> */}
-                <Transition show={showNotif} >
+                <Transition
+                    enter="transform ease-out duration-300 transition"
+                    enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+                    enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+                    leave="transition ease-in duration-100"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                    show={showNotif}
+                >
                     <div className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
                         <div className="p-4">
                             <div className="flex items-start">
