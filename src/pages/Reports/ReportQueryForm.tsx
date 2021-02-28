07418b0c606f2392ea4b0145999d9b55570fc151
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 15:33:23 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/ReportQueryForm.tsx
+++ src/pages/Reports/ReportQueryForm.tsx
@@ -9,6 +9,9 @@ const ReportQueryForm = (props: Props) => {
     return (
         <Fragment>
             <div className="bg-white mx-5 py-3 mt-4 shadow-md rounded-md">
+                <div className="mx-5">
+                    <h1>Report Generator</h1>
+                </div>
                 <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-2">
                     <Input label="Reinsurer" />
                     <Input label="Business Type" />
