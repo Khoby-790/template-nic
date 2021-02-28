a15763dbf1f40ced94ab9a93b281ce13dee18027
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 15:39:56 +0000

    --- Auto-Git Commit ---

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -2,14 +2,24 @@ import React, { Fragment } from 'react'
 
 interface Props {
     label?: string;
-    options?: any[]
+    options?: Option[]
 }
-const Select = ({ label }: Props) => {
+
+type Option = {
+    value: any;
+    label: string
+}
+const Select = ({ label, options }: Props) => {
     return (
         <Fragment>
             <div className="flex flex-col">
                 <label htmlFor="">{label}</label>
-                <select className="border border-gray-300 h-11 px-3 outline-none focus:outline-none" name="" id=""></select>
+                <select className="border placeholder-gray-400 border-gray-300 h-11 px-3 outline-none focus:outline-none" name="" id="">
+                    <option value="">choose one..</option>
+                    {options?.map(({ label, value }, key) => (
+                        <option key={key} value={value}>{label}</option>
+                    ))}
+                </select>
             </div>
         </Fragment>
     )
