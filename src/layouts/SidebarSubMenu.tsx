f9b989c2c3d66b0c578cfd7f6d75e9810e4062c7
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 13:48:01 +0000

    --- Auto-Git Commit ---

--- src/layouts/SidebarSubMenu.tsx
+++ src/layouts/SidebarSubMenu.tsx
@@ -27,9 +27,10 @@ const SidebarSubMenu = (props: Props) => {
                 leaveFrom="opacity-100 translate-x-0 sm:scale-100"
                 leaveTo="-translate-x-full"
             >
-                <aside ref={menuRef} className="hidden absolute top-0 bottom-0 overflow-hidden lg:block lg:flex-shrink-0 lg:order-first">
+                <aside ref={menuRef} className="hidden absolute top-16 bottom-0 overflow-hidden lg:block lg:flex-shrink-0 lg:order-first">
                     <div className="h-full relative flex flex-col w-64 border-r border-gray-200 bg-gray-200">
                         {/* <!-- Your content --> */}
+                        <div className="bg-transparent h-16"></div>
                         {isOpen ? "Open" : "Close"}
                     </div>
                 </aside>
