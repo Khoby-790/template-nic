836ab756aaff695a3bcc443efd81856d32b529b1
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 08:57:41 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/OfferLisitngItem.tsx
+++ src/pages/Dashboard/components/OfferLisitngItem.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react'
-import { Modal } from '../../../components'
+import { Modal, Transition } from '../../../components'
 
 interface Props {
     index: number
@@ -72,18 +72,20 @@ const OfferLisitngItem = (props: Props) => {
       From: "transform opacity-100 scale-100"
       To: "transform opacity-0 scale-95"
   --> */}
-                            <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">License</a>
-                                    <form method="POST" action="#">
-                                        <button type="submit" className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
-                                            Sign out
-        </button>
-                                    </form>
+                            <Transition>
+                                <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                                    <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
+                                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
+                                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">License</a>
+                                        <form method="POST" action="#">
+                                            <button type="submit" className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                                                Sign out
+                                            </button>
+                                        </form>
+                                    </div>
                                 </div>
-                            </div>
+                            </Transition>
                         </div>
 
                     </Fragment>
