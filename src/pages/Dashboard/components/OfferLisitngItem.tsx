09b8cac968b55263c32a7fc45a885591e95e965e
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 09:09:55 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/OfferLisitngItem.tsx
+++ src/pages/Dashboard/components/OfferLisitngItem.tsx
@@ -71,7 +71,7 @@ const OfferLisitngItem = (props: Props) => {
 
             <Fragment>
                 {/* <!-- This example requires Tailwind CSS v2.0+ --> */}
-                <div className="relative text-left">
+                <div className="absolute text-left">
                     <div>
 
                     </div>
@@ -85,7 +85,7 @@ const OfferLisitngItem = (props: Props) => {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                     >
-                        <div ref={ref} className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                        <div ref={ref} className="origin-top-right relative right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                             <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
                                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
