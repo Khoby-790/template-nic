67c009a41bd3d1efc88fe980ac4c917e82d89e37
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 08:55:30 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/OfferLisitngItem.tsx
+++ src/pages/Dashboard/components/OfferLisitngItem.tsx
@@ -72,7 +72,7 @@ const OfferLisitngItem = (props: Props) => {
       From: "transform opacity-100 scale-100"
       To: "transform opacity-0 scale-95"
   --> */}
-                            <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                            <div className="origin-top-right relative right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                                 <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
