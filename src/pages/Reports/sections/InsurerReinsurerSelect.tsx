574547f0f9beb661dec4ad5db71762bb231a7922
Khoby-790 <ebaidoo79@gmail.com>
2021-03-02 17:33:43 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/Reports/sections/InsurerReinsurerSelect.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const InsurerReinsurerSelect = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default InsurerReinsurerSelect
