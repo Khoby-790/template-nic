beb828bd58a4f904438a2bd1ccc017755f3280a9
Khoby-790 <ebaidoo79@gmail.com>
2021-03-02 11:37:59 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/ReportQueryForm.tsx
+++ src/pages/Reports/ReportQueryForm.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { Input, Select } from '../../components'
-
+import Calendar from 'react-calendar'
 interface Props {
 
 }
@@ -13,22 +13,29 @@ const ReportQueryForm = (props: Props) => {
                     <h1 className="font-bold text-xl">Report Generator</h1>
                     <p className="font-light">Provide essential information to generate report based on realtime data</p>
                 </div>
-                <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-2">
-                    <Select label="Reinsurer" />
-                    <Select label="Business Type" />
-                </div>
-                {/* <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-2">
+                <div className="flex flex-row">
+                    <div className="w-1/3 px-3">
+                        <Calendar />
+                    </div>
+                    <div className="flex-1">
+                        <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-2">
+                            <Select label="Reinsurer" />
+                            <Select label="Business Type" />
+                        </div>
+                        {/* <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-2">
                     <Input label="Reinsurer" />
                     <Input label="Business Type" />
                 </div> */}
-                <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-2">
-                    <Input type="date" label="From" />
-                    <Input type="date" label="To" />
-                </div>
-                <div className="flex mx-5 flex-1 mt-4">
-                    <button className="bg-green-600 text-white outline-none focus:outline-none flex-1 py-2 flex items-center justify-center">
-                        Generate report
+                        <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-2">
+                            <Input type="date" label="From" />
+                            <Input type="date" label="To" />
+                        </div>
+                        <div className="flex mx-5 flex-1 mt-4">
+                            <button className="bg-green-600 text-white outline-none focus:outline-none flex-1 py-2 flex items-center justify-center">
+                                Generate report
                     </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Fragment>
