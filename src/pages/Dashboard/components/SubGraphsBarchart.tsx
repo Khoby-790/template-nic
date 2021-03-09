c57a60cb8eb34d58d335a58861757824ff4d0741
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 19:46:12 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/SubGraphsBarchart.tsx
+++ src/pages/Dashboard/components/SubGraphsBarchart.tsx
@@ -6,13 +6,59 @@ interface Props {
 }
 
 
+const getIntroOfPage = (label: any) => {
+    if (label === "Users") {
+        return "Users is about number of users";
+    }
+    if (label === "Orders") {
+        return "Overview of incoming orders";
+    }
+    if (label === "Clients") {
+        return "Prospective clients";
+    }
+    if (label === "Revenue") {
+        return "Gross net worth";
+    }
+    if (label === "Other") {
+        return "Other items";
+    }
+};
+
+const CustomTooltip = ({ active, payload, label }: any) => {
+    if (active) {
+        return (
+            <div className="custom-tooltip">
+                <p className="label">{`${label} : ${payload[0].value}`}</p>
+                <p className="intro">{getIntroOfPage(label)}</p>
+                <p className="desc">Anything you want can be displayed here.</p>
+            </div>
+        );
+    }
+
+    return null;
+};
+
 const SubGraphsBarchart = ({ data = [] }: Props) => {
     return (
         <Fragment>
             <ResponsiveContainer width="100%" height="100%">
-                <BarChart width={150} height={40} data={data}>
-                    <Bar dataKey="uv" fill="#8884d8" />
-                    <Tooltip />
+                <BarChart
+                    width={650}
+                    height={350}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip content={<CustomTooltip />} />
+                    <Legend />
+                    <Bar dataKey="pv" barSize={20} fill="#8884d8" />
                 </BarChart>
             </ResponsiveContainer>
         </Fragment>
