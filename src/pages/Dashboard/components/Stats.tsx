086335074eebf08c87899975c91063639da67706
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 19:20:34 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/Dashboard/components/Stats.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const Stats = (props: Props) => {
+    return (
+        <div className="grid grid-cols-4 gap-3">
+
+        </div>
+    )
+}
+
+export default Stats
