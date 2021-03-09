445ea866ad65fad1ba2070d75050ee2b0ab89b94
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 19:49:28 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/SubGraphsPieChart.tsx
+++ src/pages/Dashboard/components/SubGraphsPieChart.tsx
@@ -1,23 +1,67 @@
 import React, { Fragment } from 'react'
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
-const data = [
+const data_ = [
   { name: 'Group A', value: 400 },
   { name: 'Group B', value: 300 },
   { name: 'Group C', value: 300 },
   { name: 'Group D', value: 200 },
 ];
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const RADIAN = Math.PI / 180;
 
 interface Props {
   data: any[]
 }
 
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}: any) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
 const SubGraphsPieChart = ({ data = [] }: Props) => {
   return (
     <Fragment>
-
+      <PieChart width={600} height={600}>
+        <Pie
+          data={data_}
+          cx={350}
+          cy={165}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={140}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
     </Fragment>
   )
 }
