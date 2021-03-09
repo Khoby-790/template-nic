9f4d86fb20b70427b050f9bd8ff280d0d85ab90c
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 19:37:29 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/Dashboard/components/Graphs/RealtimeGraph.tsx
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+interface Props {
+    data: any[]
+}
+
+const RealtimeGraph = ({ data = [] }: Props) => {
+    return (
+        <Fragment>
+            <div className="h-96 max-h-96 bg-white mx-5 my-3 py-4 shadow-md rounded-md">
+                <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                        <Area type="monotone" dataKey="pv" stroke="#D1FAE5" fill="#D1FAE5" />
+                        <Area type="monotone" dataKey="amt" stroke="#edb7dd" fill="#edb7dd" />
+                    </AreaChart>
+                </ResponsiveContainer>
+            </div>
+        </Fragment>
+    )
+}
+
+export default RealtimeGraph
