1f5e63725f90aab0479531660c720995e8e88a3d
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 19:28:44 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/Stats.tsx
+++ src/pages/Dashboard/components/Stats.tsx
@@ -30,6 +30,10 @@ const StatsPill = ({ color }: StatsPillProps) => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                 </svg>
             </div>
+            <div className="flex ml-5 flex-col">
+                <span>Title</span>
+                <span className="font-bold text-xl">Value</span>
+            </div>
         </div>
     )
 }
