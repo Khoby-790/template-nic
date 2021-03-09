231093e94b4ce166c018c6764f7fd6bed71bf6d3
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 19:27:40 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/Stats.tsx
+++ src/pages/Dashboard/components/Stats.tsx
@@ -27,7 +27,7 @@ const StatsPill = ({ color }: StatsPillProps) => {
         <div className="bg-white px-4 flex items-center flex-row py-3">
             <div className={`bg-${color}-200 rounded-lg p-4`}>
                 <svg className={`text-${color}-500 h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                 </svg>
             </div>
         </div>
