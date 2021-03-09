8ceabbedba6d357dca446c530e226729e12c1c84
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 20:06:56 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -66,7 +66,7 @@ const Dashboard = (props: Props) => {
                 <div className="flex-1 mr-3">
                     <GraphTabs data={data} />
                 </div>
-                <div className="w-1/3 h-full">
+                <div style={{ height: "54vh" }} className="w-1/3 h-full">
                     <div className=""></div>
                     <div className="bg-white px-3 pt-3 h-full w-full overflow-y-scroll">
                         <Activity />
