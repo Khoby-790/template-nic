764fc316d5c5d3b4cd2e87c860a92e498e3298a4
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 19:51:39 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/SubGraphsBarchart.tsx
+++ src/pages/Dashboard/components/SubGraphsBarchart.tsx
@@ -5,6 +5,39 @@ interface Props {
     data: any[]
 }
 
+const data_ = [
+    {
+        name: "Users",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: "Orders",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: "Clients",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: "Revenue",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: "Other",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+];
+
 
 const getIntroOfPage = (label: any) => {
     if (label === "Users") {
@@ -45,7 +78,7 @@ const SubGraphsBarchart = ({ data = [] }: Props) => {
                 <BarChart
                     width={650}
                     height={350}
-                    data={data}
+                    data={data_}
                     margin={{
                         top: 5,
                         right: 30,
