bbef3a7a8dc4306681deeadafc8507d073f5a252
Khoby-790 <ebaidoo79@gmail.com>
2021-03-09 20:00:23 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -68,8 +68,8 @@ const Dashboard = (props: Props) => {
                 </div>
                 <div className="w-1/3">
                     <div className="bg-white px-3 pt-3 h-full">
-                        <div className="flex flex-row">
-                            <div className="w-20 flex items-center justify-center">
+                        <div className="flex items-center flex-row">
+                            <div className="w-20 bg-gray-600 text-white h-20 flex items-center justify-center">
                                 <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
                                 </svg>
