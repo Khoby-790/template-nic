17ecda5b533c0030eac4dd0aa898b23fd6edeb86
Khoby-790 <ebaidoo79@gmail.com>
2021-03-11 00:45:29 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/sections/InsurerReinsurerSelect.tsx
+++ src/pages/Reports/sections/InsurerReinsurerSelect.tsx
@@ -7,24 +7,23 @@ interface Props {
 const InsurerReinsurerSelect = (props: Props) => {
     return (
         <div className="flex flex-row">
-            <div className="flex-1 bg-white mx-5 py-4 my-3 rounded-md border border-blue-500">
-                <div className="w-14 flex flex-row">
-                    <div className="w-20 flex items-center justify-center">
-                        <div className="h-11 rounded-full bg-gray-300 cursor-pointer w-11"></div>
-                    </div>
-                    <div className="h-full bg-gray-400"></div>
-                </div>
-            </div>
-            <div className="flex-1 bg-white mx-5 py-4 my-3 rounded-md border border-blue-500">
-                <div className="w-14 flex flex-row">
-                    <div className="w-20 flex items-center justify-center">
-                        <div className="h-11 rounded-full bg-gray-300 cursor-pointer w-11"></div>
-                    </div>
-                    <div className="h-full bg-gray-400"></div>
-                </div>
-            </div>
+            <Option />
+            <Option />
         </div>
     )
 }
 
 export default InsurerReinsurerSelect
+
+const Option = () => {
+    return (
+        <div className="flex-1 hover:bg-blue-200 cursor-pointer bg-white mx-5 py-4 my-3 rounded-md border border-blue-500">
+            <div className="w-14 flex flex-row">
+                <div className="w-20 flex items-center justify-center">
+                    <div className="h-11 rounded-full bg-gray-300 cursor-pointer w-11"></div>
+                </div>
+                <div className="h-full bg-gray-400"></div>
+            </div>
+        </div>
+    )
+}
