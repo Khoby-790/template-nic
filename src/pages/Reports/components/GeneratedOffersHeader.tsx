0b52f7340f4e878cb9b8d1ad7d050c80a3777430
Khoby-790 <ebaidoo79@gmail.com>
2021-03-11 01:32:25 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/components/GeneratedOffersHeader.tsx
+++ src/pages/Reports/components/GeneratedOffersHeader.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Fragment, useState, useRef } from 'react'
 import { Transition } from '../../../components'
 import { useOutsideClick } from '../../../hooks';
@@ -48,22 +49,15 @@ const GeneratedOffersHeader = (props: GeneratedOffersHeaderProps) => {
                         >
                             <div ref={saveOptionsREf} className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                 <div className="py-1" role="none">
-                                    <a href="#" className="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
-                                        {/* <!-- Heroicon name: solid/pencil-alt --> */}
-                                        <svg className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                                        </svg>
-        Edit
-      </a>
+
                                     <a href="#" className="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
                                         {/* <!-- Heroicon name: solid/duplicate --> */}
                                         <svg className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z" />
                                             <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" />
                                         </svg>
-        Duplicate
-      </a>
+                                         Duplicate
+                                    </a>
                                 </div>
                                 <div className="py-1" role="none">
                                     <a href="#" className="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
@@ -72,15 +66,15 @@ const GeneratedOffersHeader = (props: GeneratedOffersHeaderProps) => {
                                             <path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z" />
                                             <path fill-rule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clip-rule="evenodd" />
                                         </svg>
-        Archive
-      </a>
+                                        Archive
+                                    </a>
                                     <a href="#" className="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
                                         {/* <!-- Heroicon name: solid/arrow-circle-right --> */}
                                         <svg className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
                                         </svg>
-        Move
-      </a>
+                                        Move
+                                    </a>
                                 </div>
 
                             </div>
