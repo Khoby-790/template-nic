d0e155a7eb27bc6084acd96d28a518d93154d085
Khoby-790 <ebaidoo79@gmail.com>
2021-03-11 00:42:12 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/sections/InsurerReinsurerSelect.tsx
+++ src/pages/Reports/sections/InsurerReinsurerSelect.tsx
@@ -9,7 +9,7 @@ const InsurerReinsurerSelect = (props: Props) => {
         <div className="flex flex-row">
             <div className="flex-1 bg-white mx-5 py-4 my-3 rounded-md border border-blue-500">
                 <div className="w-14">
-                    <div className="h-11 bg-gray-300 w-11"></div>
+                    <div className="h-11 rounded-full  bg-gray-300 w-11"></div>
                 </div>
             </div>
         </div>
