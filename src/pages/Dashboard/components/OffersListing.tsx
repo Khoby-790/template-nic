f0a2404e46d06d2747f422da7a7065d64b670fd2
Khoby-790 <ebaidoo79@gmail.com>
2021-03-11 02:16:03 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/OffersListing.tsx
+++ src/pages/Dashboard/components/OffersListing.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { useToast } from '../../../providers/ToastProvider'
+// import { useToast } from '../../../providers/ToastProvider'
 import OfferLisitngItem from './OfferLisitngItem'
 
 interface Props {
@@ -8,10 +8,10 @@ interface Props {
 }
 
 const OffersListing = (props: Props) => {
-    const { notify } = useToast()
-    const onNotifyClicked = () => {
-        notify({ message: "Hello", description: "It is working" })
-    }
+    // const { notify } = useToast()
+    // const onNotifyClicked = () => {
+    //     notify({ message: "Hello", description: "It is working" })
+    // }
 
     return (
         <Fragment>
