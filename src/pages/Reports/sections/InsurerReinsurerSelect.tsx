d8e7e43d6e801197292f40ea943ae8a80e68afbb
Khoby-790 <ebaidoo79@gmail.com>
2021-03-11 00:46:34 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/sections/InsurerReinsurerSelect.tsx
+++ src/pages/Reports/sections/InsurerReinsurerSelect.tsx
@@ -17,10 +17,10 @@ export default InsurerReinsurerSelect
 
 const Option = () => {
     return (
-        <div className="flex-1 hover:bg-blue-200 cursor-pointer bg-white mx-5 py-4 my-3 rounded-md border border-blue-500">
+        <div className="flex-1 hover:bg-blue-200 hover:border-blue-200 cursor-pointer bg-white mx-5 py-4 my-3 rounded-md border border-blue-500">
             <div className="w-14 flex flex-row">
                 <div className="w-20 flex items-center justify-center">
-                    <div className="h-11 rounded-full bg-gray-300 cursor-pointer w-11"></div>
+                    <div className="h-11 rounded-full border border-blue-400  cursor-pointer w-11"></div>
                 </div>
                 <div className="h-full bg-gray-400"></div>
             </div>
