8a73198f41c9d7c8805af3ea3980589f4fe0227a
Khoby-790 <ebaidoo79@gmail.com>
2021-03-11 01:18:13 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/components/GeneratedOffersHeader.tsx
+++ src/pages/Reports/components/GeneratedOffersHeader.tsx
@@ -9,13 +9,15 @@ const GeneratedOffersHeader = (props: GeneratedOffersHeaderProps) => {
     return (
         <Fragment>
             <div className="flex h-11 justify-between items-center my-3 mb-5">
-                <div className="flex-1 px-2 ">
-                    <h2 className="text-2xl font-medium">{props.header}</h2>
-                    <p className="font-light">{props.description}</p>
-                    {/* <button onClick={onNotifyClicked}>Click me</button> */}
-                </div>
-                <div className="flex-1 flex justify-end px-2">
-                    <input placeholder="search" type="search" className="border-gray-200 focus:outline-none px-3 border h-12 w-1/2" />
+                <div className="flex-1 flex justify-between items-center">
+                    <div className="flex-1 px-2 ">
+                        <h2 className="text-2xl font-medium">{props.header}</h2>
+                        <p className="font-light">{props.description}</p>
+                        {/* <button onClick={onNotifyClicked}>Click me</button> */}
+                    </div>
+                    <div className="flex-1 flex justify-end px-2">
+                        <input placeholder="search" type="search" className="border-gray-200 focus:outline-none px-3 border h-12 w-1/2" />
+                    </div>
                 </div>
             </div>
         </Fragment>
