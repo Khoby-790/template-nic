6f8d627d278d463956e5a5218d18b395789fd576
Khoby-790 <ebaidoo79@gmail.com>
2021-03-11 01:07:18 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/ReportQueryForm.tsx
+++ src/pages/Reports/ReportQueryForm.tsx
@@ -23,8 +23,11 @@ const ReportQueryForm = (props: Props) => {
                         <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-1">
                             <Select label="Business Type" />
                         </div>
-                        <div className="flex mx-5 flex-1 mt-4">
-                            <button className="bg-green-600 text-white outline-none focus:outline-none flex-1 py-2 flex items-center justify-center">
+                        <div className="grid mx-5 grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-1">
+                            <Select label="Visible Columns" />
+                        </div>
+                        <div className="flex mx-5 flex-1 mt-5">
+                            <button className="bg-green-400 text-white outline-none focus:outline-none flex-1 py-2 flex items-center justify-center">
                                 Generate report
                             </button>
                         </div>
