aa53d625994cc40398f4604ab7838df0fa0b3988
Khoby-790 <ebaidoo79@gmail.com>
2021-03-11 01:22:35 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/components/GeneratedOffersHeader.tsx
+++ src/pages/Reports/components/GeneratedOffersHeader.tsx
@@ -16,7 +16,7 @@ const GeneratedOffersHeader = (props: GeneratedOffersHeaderProps) => {
                         {/* <button onClick={onNotifyClicked}>Click me</button> */}
                     </div>
                     <div className="flex-1 flex justify-end px-2">
-                        <input placeholder="search" type="search" className="border-gray-200 focus:outline-none px-3 border h-12 w-1/2" />
+                        <input placeholder="search" type="search" className="border-gray-200 focus:outline-none px-3 border h-10 w-1/2" />
                     </div>
                 </div>
                 <div className="w-36">
@@ -24,7 +24,7 @@ const GeneratedOffersHeader = (props: GeneratedOffersHeaderProps) => {
                     <div className="relative inline-block text-left">
                         <div>
                             <button type="button" className="inline-flex justify-center w-full h-full rounded-none border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="options-menu" aria-expanded="true" aria-haspopup="true">
-                                Options
+                                Save
       {/* <!-- Heroicon name: solid/chevron-down --> */}
                                 <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
