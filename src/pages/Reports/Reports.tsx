6d9c036ba227b3baa8d1ca55218e804e5ce14e00
Khoby-790 <ebaidoo79@gmail.com>
2021-03-14 16:12:00 +0000

    --- Auto-Git Commit ---

--- src/pages/Reports/Reports.tsx
+++ src/pages/Reports/Reports.tsx
@@ -10,11 +10,11 @@ const Reports = (props: Props) => {
     return (
         <Fragment>
             <div className="flex flex-row h-full">
-                <div className="w-96">
-                    <ReportQueryForm />
-                </div>
                 <div className="flex-1">
                     <OffersListing header="Generated Offers" description="All offers are subject to the query parameters" />
+                </div>
+                <div className="w-96">
+                    <ReportQueryForm />
                 </div>
             </div>
         </Fragment>
