ebb23421b8e558533d0bf9dae1e40d9f7e4f9f6b
Khoby-790 <ebaidoo79@gmail.com>
2021-03-16 12:51:23 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -62,7 +62,12 @@ const Dashboard = (props: Props) => {
                 <div className="flex-1 grid grid-cols-4 divide-x-2 border-gray-400 h-40">
                     <div className="flex justify-end py-10 px-11 h-full flex-col">
                         <h1 className="text-6xl text-white font-bold font-mono">12</h1>
-                        <p className="font-mono font-thin text-md text-gray-100">Pending offers</p>
+                        <div className="flex items-center">
+                            <p className="font-mono font-thin text-md text-gray-100">Pending offers</p>
+                            <svg className="h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                            </svg>
+                        </div>
                     </div>
                     <div className="flex justify-end py-10 px-11 h-full flex-col">
                         <h1 className="text-6xl text-white font-bold font-mono">12</h1>
