3610855cc611005dd4bcbae0c7afa3b440c018a0
Khoby-790 <ebaidoo79@gmail.com>
2021-03-17 16:52:55 +0000

    --- Auto-Git Commit ---

---
+++ src/components/Loader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Loader = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Loader
