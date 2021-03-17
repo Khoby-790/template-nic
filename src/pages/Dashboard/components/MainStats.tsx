0ac4792301bc6c354793a367b1370c63df1bfea3
Khoby-790 <ebaidoo79@gmail.com>
2021-03-17 16:34:23 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/components/MainStats.tsx
+++ src/pages/Dashboard/components/MainStats.tsx
@@ -34,7 +34,7 @@ const MainStats = (props: Props) => {
                     </div>
                     <div className="flex justify-end py-10 px-11 h-full flex-col">
                         <div className="flex items-baseline">
-                            <h1 className="text-6xl text-white font-bold font-mono">12</h1>
+                            <h1 className="text-6xl text-white font-bold font-mono">600</h1>
                             <svg className="h-9 text-green-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7l4-4m0 0l4 4m-4-4v18" />
                             </svg>
@@ -45,7 +45,7 @@ const MainStats = (props: Props) => {
                     </div>
                     <div className="flex justify-end py-10 px-11 h-full flex-col">
                         <div className="flex items-baseline">
-                            <h1 className="text-6xl text-white font-bold font-mono">12</h1>
+                            <h1 className="text-6xl text-white font-bold font-mono">1.2B</h1>
                             <svg className="h-9 text-green-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7l4-4m0 0l4 4m-4-4v18" />
                             </svg>
