f0a6fa730141957c47079f23ca5ebad8ef664217
Khoby-790 <ebaidoo79@gmail.com>
2021-03-17 16:58:23 +0000

    --- Auto-Git Commit ---

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -45,18 +45,9 @@ const Settings = (props: Props) => {
                                             <a onClick={() => setTab("general")} href="#" className={`${tab === "general" ? "border-purple-500" : "border-transparent"} text-purple-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}>
                                                 General
                                             </a>
-                                            <a onClick={() => setTab("general")} href="#" className={`${tab === "general" ? "border-purple-500" : "border-transparent"} text-purple-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}>
-                                                Password
-                                            </a>
-                                            <a onClick={() => setTab("general")} href="#" className={`${tab === "general" ? "border-purple-500" : "border-transparent"} text-purple-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}>
-                                                Notification
-                                            </a>
-                                            <a onClick={() => setTab("general")} href="#" className={`${tab === "general" ? "border-purple-500" : "border-transparent"} text-purple-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}>
-                                                Plan
-                                            </a>
 
-                                            <a href="#" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 whitespace-nowrap ml-8 py-4 px-1 border-b-2 font-medium text-sm">
-                                                Billing
+                                            <a onClick={() => setTab("password")} href="#" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 whitespace-nowrap ml-8 py-4 px-1 border-b-2 font-medium text-sm">
+                                                Password
                                             </a>
 
                                             <a href="#" className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 whitespace-nowrap ml-8 py-4 px-1 border-b-2 font-medium text-sm">
