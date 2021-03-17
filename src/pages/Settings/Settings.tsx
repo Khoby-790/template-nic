47119977a035af506b7d69d85a1d6fc675c88c25
Khoby-790 <ebaidoo79@gmail.com>
2021-03-17 17:10:24 +0000

    --- Auto-Git Commit ---

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -42,11 +42,11 @@ const Settings = (props: Props) => {
                                     <div className="border-b border-gray-200">
                                         <nav className="-mb-px flex">
                                             {/* <!-- Current: "border-purple-500 text-purple-600", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
-                                            <NavTab first={true} name="General" tab="general" setTab={setTab} />
-                                            <NavTab name="Password" tab="password" setTab={setTab} />
-                                            <NavTab name="Notifcations" tab="notifications" setTab={setTab} />
-                                            <NavTab name="Billing" tab="billing" setTab={setTab} />
-                                            <NavTab name="General" tab="general" setTab={setTab} />
+                                            <NavTab value="general" first={true} name="General" tab="general" setTab={setTab} />
+                                            <NavTab value="password" name="Password" tab="password" setTab={setTab} />
+                                            <NavTab value="notifications" name="Notifcations" tab="notifications" setTab={setTab} />
+                                            <NavTab value="billing" name="Billing" tab="billing" setTab={setTab} />
+                                            <NavTab value="general" name="General" tab="general" setTab={setTab} />
                                         </nav>
                                     </div>
                                 </div>
@@ -72,10 +72,11 @@ type NavTabProps = {
     tab: Tab;
     first?: boolean;
     name: string;
+    value: Tab
 }
 
 
-const NavTab = ({ setTab, tab, first = false, name }: NavTabProps) => {
+const NavTab = ({ setTab, tab, first = false, name, value }: NavTabProps) => {
     return (
         <a onClick={() => setTab(tab)} href="#" className={`${tab === "general" ? "border-purple-500 text-purple-600" : "border-transparent text-gray-500"}  ${first ? "" : "ml-8"} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}>
             {name}
