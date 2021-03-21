789bbb3d232ab3e0147d0ebab9f85942f4db4930
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 12:18:42 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/NicStatsExpansion.tsx
+++ src/pages/NicStatsExpansion/NicStatsExpansion.tsx
@@ -8,7 +8,7 @@ interface Props {
 type Tabs = "for_each_reinsurer" | "for_each_broker" | "for_all_brokers" | "for_all_reinsurers"
 
 const NicStatsExpansion = (props: Props) => {
-    const [tab, setTab] = useState<>("for_each_reinsurer")
+    const [tab, setTab] = useState<Tabs>("for_each_reinsurer")
     return (
         <Fragment>
             <div className="px-3 py-4">
@@ -125,7 +125,80 @@ const NicStatsExpansion = (props: Props) => {
                 </div>
 
                 <Fragment>
-                    { }
+                    {tab === "for_each_broker" && (
+                        <div>
+                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                                <div className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                    <div className="flex-shrink-0">
+                                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=ah3lxr8uqw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                    </div>
+                                    <div className="flex-1 min-w-0">
+                                        <a href="#" className="focus:outline-none">
+                                            <span className="absolute inset-0" aria-hidden="true"></span>
+                                            <p className="text-sm font-medium text-gray-900">
+                                                Leslie Alexander
+        </p>
+                                            <p className="text-sm text-gray-500 truncate">
+                                                Co-Founder / CEO
+        </p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                    <div className="flex-shrink-0">
+                                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixqx=ah3lxr8uqw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                    </div>
+                                    <div className="flex-1 min-w-0">
+                                        <a href="#" className="focus:outline-none">
+                                            <span className="absolute inset-0" aria-hidden="true"></span>
+                                            <p className="text-sm font-medium text-gray-900">
+                                                Michael Foster
+        </p>
+                                            <p className="text-sm text-gray-500 truncate">
+                                                Co-Founder / CTO
+        </p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                    <div className="flex-shrink-0">
+                                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixqx=ah3lxr8uqw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                    </div>
+                                    <div className="flex-1 min-w-0">
+                                        <a href="#" className="focus:outline-none">
+                                            <span className="absolute inset-0" aria-hidden="true"></span>
+                                            <p className="text-sm font-medium text-gray-900">
+                                                Dries Vincent
+        </p>
+                                            <p className="text-sm text-gray-500 truncate">
+                                                Manager, Business Relations
+        </p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                    <div className="flex-shrink-0">
+                                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixqx=ah3lxr8uqw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                    </div>
+                                    <div className="flex-1 min-w-0">
+                                        <a href="#" className="focus:outline-none">
+                                            <span className="absolute inset-0" aria-hidden="true"></span>
+                                            <p className="text-sm font-medium text-gray-900">
+                                                Lindsay Walton
+        </p>
+                                            <p className="text-sm text-gray-500 truncate">
+                                                Front-end Developer
+        </p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    )}
                 </Fragment>
 
             </div>
