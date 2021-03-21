e4f9a02ceda71bef93efa4661b033c496380eb23
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 12:01:14 +0000

    --- Auto-Git Commit ---

--- src/pages/NewDashboard/sections/NICSection.tsx
+++ src/pages/NewDashboard/sections/NICSection.tsx
@@ -55,9 +55,16 @@ const NICSection = (props: Props) => {
         <>
             <div className="bg-white shadow-lg my-4">
                 <div className="h-16 flex justify-between px-3">
-                    <div className="flex flex-col">
-                        <span className="font-semibold ">NIC</span>
-                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. </span>
+                    <div className="flex">
+                        <div className="">
+                            <svg className="h-12 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                            </svg>
+                        </div>
+                        <div className="flex flex-col">
+                            <span className="font-semibold ">NIC</span>
+                            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. </span>
+                        </div>
                     </div>
                     <div className="">
 
