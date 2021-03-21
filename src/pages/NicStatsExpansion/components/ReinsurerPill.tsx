25a56ef51554cfe396ebb6eb84fe95d63e638053
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 13:10:02 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/components/ReinsurerPill.tsx
+++ src/pages/NicStatsExpansion/components/ReinsurerPill.tsx
@@ -30,9 +30,8 @@ const ReinsurerPill = ({ name }: Props) => {
                                     <span>East Legon</span>
                                 </p>
                             </a>
-                            <div onClick={() => setShowGraph(true)} className="flex text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
-                                View
-                                <svg className="h-6 ml-3 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
+                                <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                 </svg>
