a30594a7316485c76d6e85a9274e8969c9c037fe
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 13:38:53 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/NewDashboard/sections/PremiumSection.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        name: 'Jan',
+        paid: 4000,
+        outstanding: 2400,
+        total: 2400,
+    },
+    {
+        name: 'Feb',
+        paid: 3000,
+        outstanding: 1398,
+        total: 2210,
+    },
+    {
+        name: 'Mar',
+        paid: 2000,
+        outstanding: 9800,
+        total: 2290,
+    },
+    {
+        name: 'Apr',
+        paid: 2780,
+        outstanding: 3908,
+        total: 2000,
+    },
+    {
+        name: 'May',
+        paid: 1890,
+        outstanding: 4800,
+        total: 2181,
+    },
+    {
+        name: 'Jun',
+        paid: 2390,
+        outstanding: 3800,
+        total: 2500,
+    },
+    {
+        name: 'Jul',
+        paid: 3490,
+        outstanding: 4300,
+        total: 2100,
+    },
+];
+interface Props {
+
+}
+
+const PremiumSection = (props: Props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PremiumSection
