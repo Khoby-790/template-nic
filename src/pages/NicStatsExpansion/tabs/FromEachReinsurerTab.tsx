707ecd949caecf2774cadba415753f3c9d75f704
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 23:05:34 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/tabs/FromEachReinsurerTab.tsx
+++ src/pages/NicStatsExpansion/tabs/FromEachReinsurerTab.tsx
@@ -30,8 +30,8 @@ const FromEachReinsurerTab = (props: Props) => {
                                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Total
               </th>
-                                        <th scope="col" className="relative px-6 py-3">
-                                            <span className="sr-only">Edit</span>
+                                        <th scope="col" className="relative px-6 py-3 font-medium text-gray-500 uppercase tracking-wider">
+                                            Actions
                                         </th>
                                     </tr>
                                 </thead>
@@ -96,7 +96,7 @@ const FromEachReinsurerTab = (props: Props) => {
                 <div className="-mt-px w-0 flex-1 flex">
                     <a href="#" className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
                         {/* <!-- Heroicon name: solid/arrow-narrow-left --> */}
-      <svg className="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <svg className="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                             <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
                         </svg>
       Previous
