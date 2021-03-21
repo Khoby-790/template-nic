932915d59b04fe879484b0311b725ff830b84d07
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 13:50:50 +0000

    --- Auto-Git Commit ---

--- src/pages/NewDashboard/NewDashboard.tsx
+++ src/pages/NewDashboard/NewDashboard.tsx
@@ -11,7 +11,12 @@ interface Props {
 const NewDashboard = (props: Props) => {
     return (
         <div className="px-3 py-4">
-            <WelcomeSection />
+            <div className="flex">
+                <div className="flex-1">
+                    <WelcomeSection />
+                </div>
+                <div className="flex-1"></div>
+            </div>
             <NICSection />
             <PremiumSection />
         </div>
