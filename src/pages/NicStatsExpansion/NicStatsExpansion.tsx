d8a3bcf6c8de39d73f81297f6d471abee6f00d21
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 12:44:52 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/NicStatsExpansion.tsx
+++ src/pages/NicStatsExpansion/NicStatsExpansion.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
 import ForEachReinsurerTab from './tabs/ForEachReinsurerTab'
 import FromEachReinsurerTab from './tabs/FromEachReinsurerTab';
 
@@ -37,7 +38,7 @@ const NicStatsExpansion = (props: Props) => {
                                     <svg className="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
                                     </svg>
-                                    <a href="#" className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Dashboard</a>
+                                    <Link to="/"  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Dashboard</Link>
                                 </div>
                             </li>
                             <li className="flex">
@@ -45,7 +46,7 @@ const NicStatsExpansion = (props: Props) => {
                                     <svg className="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
                                     </svg>
-                                    <a href="#" aria-current="page" className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">NIC Summary</a>
+                                    <span  aria-current="page" className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">NIC Summary</span>
                                 </div>
                             </li>
                         </ol>
