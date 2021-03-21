5863be41803a9d99c24694c3ea58a7fa8e228150
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 22:15:50 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/PremiumExpansion/PremiumExpansion.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { PageBreadCrum } from '../../components';
+
+interface Props {
+
+}
+
+
+const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
+const active = "border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
+
+type Tabs = "for_each_reinsurer" | "for_each_broker" | "for_all_brokers" | "for_all_reinsurers"
+
+const PremiumExpansion = (props: Props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PremiumExpansion
