5ee5746b1b91d57fd68ef1cc9ce16fe43139c7b8
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 22:13:06 +0000

    --- Auto-Git Commit ---

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Transition } from "./Transition";
 export { default as Modal } from "./Modal";
 export { default as Input } from "./Input";
 export { default as Select } from "./Select";
+export { default as PageBreadCrum } from "./PageBreadCrum";
