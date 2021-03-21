8a71d6a723967500a74e6cc90b404ebf295829b1
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 11:48:09 +0000

    --- Auto-Git Commit ---

--- src/pages/NewDashboard/NewDashboard.tsx
+++ src/pages/NewDashboard/NewDashboard.tsx
@@ -1,6 +1,52 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import WelcomeSection from './sections/WelcomeSection'
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        name: 'Jan',
+        paid: 4000,
+        outstanding: 2400,
+        total: 2400,
+    },
+    {
+        name: 'Feb',
+        paid: 3000,
+        outstanding: 1398,
+        total: 2210,
+    },
+    {
+        name: 'Mar',
+        paid: 2000,
+        outstanding: 9800,
+        total: 2290,
+    },
+    {
+        name: 'Apr',
+        paid: 2780,
+        outstanding: 3908,
+        total: 2000,
+    },
+    {
+        name: 'May',
+        paid: 1890,
+        outstanding: 4800,
+        total: 2181,
+    },
+    {
+        name: 'Jun',
+        paid: 2390,
+        outstanding: 3800,
+        total: 2500,
+    },
+    {
+        name: 'Jul',
+        paid: 3490,
+        outstanding: 4300,
+        total: 2100,
+    },
+];
 
 interface Props {
 
@@ -21,7 +67,29 @@ const NewDashboard = (props: Props) => {
                     </div>
                 </div>
                 <div className="flex h-64">
-                    <div className="flex-1"></div>
+                    <div className="flex-1">
+                        <ResponsiveContainer width="100%" height="100%">
+                            <BarChart
+                                width={500}
+                                height={300}
+                                data={data}
+                                margin={{
+                                    top: 5,
+                                    right: 30,
+                                    left: 20,
+                                    bottom: 5,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Legend />
+                                <Bar dataKey="pv" fill="#8884d8" />
+                                <Bar dataKey="uv" fill="#82ca9d" />
+                            </BarChart>
+                        </ResponsiveContainer>
+                    </div>
                     <div className="w-64">
                         <div className="flex flex-col">
                             <span className="font-bold text-2xl text-gray-600">12000</span>
