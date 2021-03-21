8da497003ab7593cb87c18e8da19c79725bc8639
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 23:19:41 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/NicStatsExpansion.tsx
+++ src/pages/NicStatsExpansion/NicStatsExpansion.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Fragment, useState } from 'react'
 import { PageBreadCrum } from '../../components';
+import SubGraphsBarchart from '../Dashboard/components/SubGraphsBarchart';
 import ForEachReinsurerTab from './tabs/ForEachReinsurerTab'
 import FromEachReinsurerTab from './tabs/FromEachReinsurerTab';
 
@@ -8,11 +9,91 @@ interface Props {
 
 }
 
+const data = [
+    {
+        name: 'Jan',
+        paid: 4000,
+        outstanding: 2400,
+        total: 2400,
+    },
+    {
+        name: 'Feb',
+        paid: 3000,
+        outstanding: 1398,
+        total: 2210,
+    },
+    {
+        name: 'Mar',
+        paid: 2000,
+        outstanding: 9800,
+        total: 2290,
+    },
+    {
+        name: 'Apr',
+        paid: 2780,
+        outstanding: 3908,
+        total: 2000,
+    },
+    {
+        name: 'May',
+        paid: 1890,
+        outstanding: 4800,
+        total: 2181,
+    },
+    {
+        name: 'Jun',
+        paid: 2390,
+        outstanding: 3800,
+        total: 2500,
+    },
+    {
+        name: 'Jul',
+        paid: 3490,
+        outstanding: 4300,
+        total: 2100,
+    },
+    {
+        name: 'Aug',
+        paid: 4000,
+        outstanding: 2400,
+        total: 2400,
+    },
+    {
+        name: 'Sep',
+        paid: 3000,
+        outstanding: 1398,
+        total: 2210,
+    },
+    {
+        name: 'Oct',
+        paid: 2000,
+        outstanding: 9800,
+        total: 2290,
+    },
+    {
+        name: 'Nov',
+        paid: 2780,
+        outstanding: 3908,
+        total: 2000,
+    },
+    {
+        name: 'Dec',
+        paid: 1890,
+        outstanding: 4800,
+        total: 2181,
+    },
+];
 
 const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
 const active = "border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
 
 type Tabs = "for_each_reinsurer" | "for_each_broker" | "for_all_brokers" | "for_all_reinsurers"
+
+const bars = [
+    { name: "paid", color: "#BA5624", title: "Total Paid Premium" },
+    { name: "outstanding", color: "#381D2A", title: "Total Outstanding Premium" },
+    { name: "total", color: "#FCDE9C", title: "Total Premium" }
+]
 
 const NicStatsExpansion = (props: Props) => {
     const [tab, setTab] = useState<Tabs>("for_each_reinsurer")
