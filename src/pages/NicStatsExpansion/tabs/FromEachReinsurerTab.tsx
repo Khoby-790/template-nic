8f9e8f683d0ed80a5fbee233764943dbbfb30cb6
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 12:56:56 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/tabs/FromEachReinsurerTab.tsx
+++ src/pages/NicStatsExpansion/tabs/FromEachReinsurerTab.tsx
@@ -51,7 +51,7 @@ const FromEachReinsurerTab = (props: Props) => {
                                             Admin
               </td>
                                         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <svg className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <svg onClick={() => setShowGraph(true)} className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                                             </svg>
                                         </td>
@@ -87,7 +87,7 @@ const FromEachReinsurerTab = (props: Props) => {
             </div>
 
 
-            <Modal size={50} show={showGraph} setShow={setShowGraph}>
+            <Modal size={80} show={showGraph} setShow={setShowGraph}>
                 <DisplayBarChart />
             </Modal>
 
