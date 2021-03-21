14ab6720dfb78a29a6ddab635fddf6a0b98f6741
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 15:42:49 +0000

    --- Auto-Git Commit ---

--- src/layouts/AppContent.tsx
+++ src/layouts/AppContent.tsx
@@ -15,10 +15,8 @@ const AppContent = (props: Props) => {
     return (
         <Fragment>
             <main className="min-w-0 flex-1 border-t border-gray-200 lg:flex">
-                {/* <!-- Primary column --> */}
                 <section aria-labelledby="primary-heading" className="min-w-0 transition duration-500 flex-1 h-full flex flex-col overflow-y-scroll lg:order-last">
                     <h1 id="primary-heading" className="sr-only">Home</h1>
-                    {/* <!-- Your content --> */}
                     <Suspense fallback={<h1>Loading ...</h1>}>
                         <Switch>
                             <Route path="/" exact component={NewDashboard} />
@@ -29,8 +27,6 @@ const AppContent = (props: Props) => {
                         </Switch>
                     </Suspense>
                 </section>
-
-                {/* <!-- Secondary column (hidden on smaller screens) --> */}
                 <SidebarSubMenu />
             </main>
         </Fragment>
