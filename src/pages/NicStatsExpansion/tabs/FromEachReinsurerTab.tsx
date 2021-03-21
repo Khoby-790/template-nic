23c3c4022dc883f25ac5dd74882789377e29c303
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 12:51:24 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/tabs/FromEachReinsurerTab.tsx
+++ src/pages/NicStatsExpansion/tabs/FromEachReinsurerTab.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Modal } from '../../../components'
 
 interface Props {
 
 }
 
 const FromEachReinsurerTab = (props: Props) => {
+    const [showGraph, setShowGraph] = useState<boolean>(false)
     return (
         <div className="py-4">
             {/* <!-- This example requires Tailwind CSS v2.0+ --> */}
@@ -69,7 +71,7 @@ const FromEachReinsurerTab = (props: Props) => {
                                             Owner
               </td>
                                         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <svg className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <svg onClick={() => setShowGraph(true)} className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                                             </svg>
                                         </td>
@@ -82,6 +84,11 @@ const FromEachReinsurerTab = (props: Props) => {
                     </div>
                 </div>
             </div>
+
+
+            <Modal show={showGraph} setShow={setShowGraph}>
+                Hello
+            </Modal>
 
         </div>
     )
