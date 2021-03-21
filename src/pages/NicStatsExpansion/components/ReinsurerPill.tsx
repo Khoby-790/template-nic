7da49901a487de6d6099a09713419f18a9bd188e
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 13:07:47 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/components/ReinsurerPill.tsx
+++ src/pages/NicStatsExpansion/components/ReinsurerPill.tsx
@@ -15,7 +15,7 @@ const ReinsurerPill = ({ name }: Props) => {
                 <div className="flex-shrink-0">
                     <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=ah3lxr8uqw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                 </div>
-                <div className="flex-1 min-w-0">
+                <div className="flex-1 flex flex-col min-w-0">
                     <div className="flex">
                         <div className="">
                             <a href="#" className="focus:outline-none">
@@ -30,14 +30,9 @@ const ReinsurerPill = ({ name }: Props) => {
                                     <span>East Legon</span>
                                 </p>
                             </a>
-                            <div onClick={() => setShowGraph(true)} className="flex text-gray-500 items-center hover:bg-indigo-600 my-3">
-                                View
-                                <svg className="h-6 ml-3 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                </svg>
-                            </div>
+
                         </div>
+
                         <div className="px-5 grid grid-cols-3 gap-x-5">
                             <div className="flex flex-col">
                                 <span className="font-thin">Total</span>
@@ -52,6 +47,13 @@ const ReinsurerPill = ({ name }: Props) => {
                                 <span className="font-semibold text-xl">12,022,152</span>
                             </div>
                         </div>
+                    </div>
+                    <div onClick={() => setShowGraph(true)} className="flex text-gray-500 items-center hover:bg-indigo-600 my-3">
+                        View
+                                <svg className="h-6 ml-3 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
                     </div>
                 </div>
             </div>
