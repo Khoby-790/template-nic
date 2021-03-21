14344621b693d25f35c2cc00aa7a7746f9b7b148
Khoby-790 <ebaidoo79@gmail.com>
2021-03-21 12:29:36 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/tabs/ForEachReinsurerTab.tsx
+++ src/pages/NicStatsExpansion/tabs/ForEachReinsurerTab.tsx
@@ -10,7 +10,9 @@ const ForEachReinsurerTab = (props: Props) => {
     return (
         <div className="py-4">
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                <ReinsurerPill />
+                <ReinsurerPill name="Visal Reinsurance Company" />
+                <ReinsurerPill name="Kek Reinsurance Company" />
+                <ReinsurerPill name="iRisk Reinsurance Company" />
             </div>
 
         </div>
