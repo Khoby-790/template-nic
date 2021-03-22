43c63cf6cc07ec6136ae729f35bf7054b8d443e0
Khoby-790 <ebaidoo79@gmail.com>
2021-03-22 12:20:58 +0000

    --- Auto-Git Commit ---

--- src/pages/NewDashboard/sections/OffersPlaced.jsx
+++ src/pages/NewDashboard/sections/OffersPlaced.jsx
@@ -23,13 +23,14 @@ const OffersPlaced = () => {
       <Ringcomponent data={myData} content={myContent} />
       <Ringcomponent data={myData} content={content2} />
       <div className="mx-3 py-4 px-5 flex-col flex-1 flex justify-center items-center">
+        <div className="flex justify-end">Hello</div>
         <div className="flex mb-4 flex-col">
           <span className="text-2xl font-semibold">78,564</span>
-          <span className="font-thin">Total</span>
+          <span className="font-light text-gray-400">Current</span>
         </div>
         <div className="flex flex-col">
           <span className="text-2xl font-semibold">78,564</span>
-          <span>Total</span>
+          <span className="font-light text-gray-400">Total</span>
         </div>
       </div>
     </div>
