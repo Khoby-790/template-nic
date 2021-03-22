980c4088267728fc9b8f8ca82709bea416b4659f
Khoby-790 <ebaidoo79@gmail.com>
2021-03-22 10:59:00 +0000

    --- Auto-Git Commit ---

---
+++ src/components/CustomeAreaChart.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer, AreaProps } from 'recharts';
+import { CurveType } from 'recharts/types/shape/Curve';
+
+interface Props {
+    data: any[];
+    lines: AreaProps[]; 
+}
+
+const CustomeAreaChart = ({ data }: Props) => {
+    return (
+        <div>
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                    width={500}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    {/* <CartesianGrid strokeDasharray="3 3" /> */}
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                    <Area type="monotone" dataKey="pv" stroke="#D1FAE5" fill="#D1FAE5" />
+                    <Area type="monotone" dataKey="amt" stroke="#edb7dd" fill="#edb7dd" />
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
+
+export default CustomeAreaChart
