c8d7a1bcee028207a231085ca018d2dd34136286
Khoby-790 <ebaidoo79@gmail.com>
2021-03-22 12:22:03 +0000

    --- Auto-Git Commit ---

--- src/pages/NewDashboard/sections/OffersPlaced.jsx
+++ src/pages/NewDashboard/sections/OffersPlaced.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Ringcomponent from "../components/Ringcomponent";
 
 const myData = [
@@ -23,7 +24,27 @@ const OffersPlaced = () => {
       <Ringcomponent data={myData} content={myContent} />
       <Ringcomponent data={myData} content={content2} />
       <div className="mx-3 py-4 px-5 flex-col flex-1 flex justify-center items-center">
-        <div className="flex justify-end">Hello</div>
+        <div className="flex justify-end w-full">
+          <Link
+            to="/claims"
+            className=" flex justify-center h-full items-center px-4 cursor-pointer hover:bg-gray-400 rounded-full"
+          >
+            <svg
+              className="h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+          </Link>
+        </div>
         <div className="flex mb-4 flex-col">
           <span className="text-2xl font-semibold">78,564</span>
           <span className="font-light text-gray-400">Current</span>
