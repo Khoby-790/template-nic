c2c7d6791030a0a7231523c90207d9d8d720f67b
Khoby-790 <ebaidoo79@gmail.com>
2021-03-22 16:14:37 +0000

    --- Auto-Git Commit ---

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import ClipLoader from "react-spinners/ClipLoader";
 
 interface Props {
-    
+
 }
 
 const Loader = (props: Props) => {
     return (
-        <div>
-            
+        <div className="h-full flex items-center justify-center">
+            <ClipLoader color={"#2364AA"} loading={true} size={150} />
         </div>
     )
 }
