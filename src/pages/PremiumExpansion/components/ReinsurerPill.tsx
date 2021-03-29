cebd735bcd1e7a3e81e28177ddaedac74ff8760a
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 22:30:25 +0000

    --- Auto-Git Commit ---

--- src/pages/PremiumExpansion/components/ReinsurerPill.tsx
+++ src/pages/PremiumExpansion/components/ReinsurerPill.tsx
@@ -27,7 +27,10 @@ const ReinsurerPill = ({ name }: Props) => {
                                     <svg className="h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                                     </svg>
-                                    <span>East Legon</span>
+                                    <div className="flex flex-col">
+                                        <span>Address, </span>
+                                        <span>contact details</span>
+                                    </div>
                                 </p>
                             </a>
                             <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
