5e0367782fe20bacde3cb8a9ca83b002fd9b5abf
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 23:21:47 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/NicStatsExpansion.tsx
+++ src/pages/NicStatsExpansion/NicStatsExpansion.tsx
@@ -87,7 +87,7 @@ const data = [
 ];
 
 
-const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
+const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm";
 const active = "border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
 
 type Tabs = "for_each_reinsurer" | "for_each_broker" | "for_all_brokers" | "for_all_reinsurers"
