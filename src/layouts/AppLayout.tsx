ca1313552eac00afdc9e38e7548b990f9d453009
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 21:15:08 +0000

    --- Auto-Git Commit ---

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -15,22 +15,11 @@ type Props = RouteComponentProps<any, StaticContext, unknown>
 const AppLayout = ({ history, match }: Props) => {
     return (
         <Fragment>
-
             <div className="h-screen flex">
                 <Sidebar />
-                <div className="flex-1 h-screen">
+                <div className="flex-1 h-full">
                     <Navbar />
                     <div className="bg-red-500 flex-1"></div>
-                </div>
-            </div>
-
-            <div className="h-screen overflow-hidden bg-gray-100 flex flex-col">
-                {/* <!-- Top nav--> */}
-                {/* <!-- Bottom section --> */}
-                <div className="min-h-0 flex-1 flex overflow-hidden">
-                    {/* <!-- Narrow sidebar--> */}
-                    {/* <Sidebar /> */}
-                    {/* <!-- Main area --> */}
                 </div>
             </div>
         </Fragment>
