74853fde933b3e484f12e9d099426bf006dbc4cc
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 22:59:56 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/NicStatsExpansion/tabs/ReinsurersList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const ReinsurersList = (props: Props) => {
+    return (
+        <div className="py-4">
+            
+        </div>
+    )
+}
+
+export default ReinsurersList
