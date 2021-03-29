bd613a2aee07f8a4fd5aa96027f391661143cebd
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 23:13:03 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/tabs/ReinsurersList.tsx
+++ src/pages/NicStatsExpansion/tabs/ReinsurersList.tsx
@@ -45,7 +45,7 @@ const ReinsurersList = (props: Props) => {
                         <input placeholder="search" type="search" className="border-gray-200 focus:outline-none px-3 border h-10 w-1/2" />
                     </div>
                 </div>
-                <div className="w-20">
+                <div className="w-auto">
                     <div className="relative inline-block text-left">
                         <div>
                             <button type="button" onClick={() => setShowSaveOptions(true)} className="inline-flex justify-center w-full h-full rounded-none border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="options-menu" aria-expanded="true" aria-haspopup="true">
