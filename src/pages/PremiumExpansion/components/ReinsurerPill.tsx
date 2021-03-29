b81f7e11f3e79fc699f5f5474c7c797fc52ebea8
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 22:32:36 +0000

    --- Auto-Git Commit ---

--- src/pages/PremiumExpansion/components/ReinsurerPill.tsx
+++ src/pages/PremiumExpansion/components/ReinsurerPill.tsx
@@ -33,18 +33,21 @@ const ReinsurerPill = ({ name }: Props) => {
                                     </div>
                                 </p>
                             </a>
-                            <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
-                                <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                </svg>
+                            <div className="flex">
+                                <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
+                                    <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                </div>
+                                <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
+                                    <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                    </svg>
+                                </div>
                             </div>
-                            <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
-                                <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                </svg>
-                            </div>
+
+
                         </div>
                         <div>
                             <div className="py-3 flex justify-end">
