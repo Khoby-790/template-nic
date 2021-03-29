c6a5892520bde806f60c719ffbee5d7626074428
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 22:31:31 +0000

    --- Auto-Git Commit ---

--- src/pages/PremiumExpansion/components/ReinsurerPill.tsx
+++ src/pages/PremiumExpansion/components/ReinsurerPill.tsx
@@ -23,16 +23,22 @@ const ReinsurerPill = ({ name }: Props) => {
                                 <p className="text-sm font-medium text-gray-900">
                                     {name || "Leslie Alexander"}
                                 </p>
-                                <p className="text-sm flex items-center text-gray-500 truncate">
-                                    <svg className="h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <p className="text-sm flex items-start text-gray-500 truncate">
+                                    <svg className="h-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                                     </svg>
                                     <div className="flex flex-col">
-                                        <span>Address, </span>
-                                        <span>contact details</span>
+                                        <span>Address </span>
+                                        <span>Contact Details</span>
                                     </div>
                                 </p>
                             </a>
+                            <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
+                                <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                </svg>
+                            </div>
                             <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
                                 <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
