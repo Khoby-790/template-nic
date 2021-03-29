e1b81932941c65b258f2c360c0c21c2c7348e438
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 22:26:03 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/tabs/ForAllBrokers.tsx
+++ src/pages/NicStatsExpansion/tabs/ForAllBrokers.tsx
@@ -9,9 +9,10 @@ const ForAllBrokers = (props: Props) => {
     return (
         <div className="py-4">
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                <ReinsurerPill name="Visal Reinsurance Company" />
-                <ReinsurerPill name="Kek Reinsurance Company" />
-                <ReinsurerPill name="iRisk Reinsurance Company" />
+                <ReinsurerPill name="Visal Reinsurance Brokers" />
+                <ReinsurerPill name="Kek Reinsurance Brokers" />
+                <ReinsurerPill name="iRisk Reinsurance Brokers" />
+                <ReinsurerPill name="Afro-asian Reinsurance Brokers" />
             </div>
 
         </div>
