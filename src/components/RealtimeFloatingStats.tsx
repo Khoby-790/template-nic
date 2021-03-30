af2f1b32da4ce6d11f0c11e06dd0dac028ade28b
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 00:21:55 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeFloatingStats.tsx
+++ src/components/RealtimeFloatingStats.tsx
@@ -6,6 +6,7 @@ interface Props {
 
 const RealtimeFloatingStats = (props: Props) => {
     const [time, setTime] = useState<string>(new Date().toTimeString())
+    const [currentDate] = useState<string>(new Date().toDateString())
     useEffect(() => {
         setInterval(() => {
             setTime(new Date().toTimeString())
@@ -13,15 +14,16 @@ const RealtimeFloatingStats = (props: Props) => {
     }, [])
     return (
         <Fragment>
-            <div className="absolute bottom-10 w-6/12 right-0 shadow-xl rounded-lg px-5 bg-white">
-                <div className="h-9 flex items-center justify-between">
+            <div className="absolute bottom-10 w-6/12 right-0 shadow-xl rounded-lg bg-white">
+                <div className="h-9 border-b px-5 flex items-center justify-between">
                     <span className="text-lg">Realtime Data on placed offers</span>
                     <svg className="h-6 text-gray-700 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
                     </svg>
                 </div>
-                <div className="flex">
+                <div className="flex px-5">
                     <div className="flex flex-col">
+                        <span>{currentDate}</span>
                         <span>{time}</span>
                     </div>
                 </div>
