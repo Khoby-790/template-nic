b7fb9faee1242b56b1b26454258c46e0856549d9
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 14:17:07 +0000

    --- Auto-Git Commit ---

--- src/pages/PremiumExpansion/tabs/FromEachReinsuredTab.tsx
+++ src/pages/PremiumExpansion/tabs/FromEachReinsuredTab.tsx
@@ -17,7 +17,7 @@ const FromEachReinsuredTab = (props: Props) => {
             <div className="h-auto px-4 py-3 flex justify-between">
                 <div className="flex flex-col">
                     <span className="text-xl font-bold">Summary</span>
-                    <span className="font-light">Total NIC Levies for 2021</span>
+                    <span className="font-light">Total Premium for 2021</span>
                 </div>
                 <div className="flex">
                     <div className="flex border  mr-2 p-2 mb-1 rounded items-baseline">
