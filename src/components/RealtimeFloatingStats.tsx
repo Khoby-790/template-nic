37ae29274da1246c78133c1a7c947afde711a8a7
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 00:35:01 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeFloatingStats.tsx
+++ src/components/RealtimeFloatingStats.tsx
@@ -14,7 +14,7 @@ const RealtimeFloatingStats = (props: Props) => {
     }, [])
     return (
         <Fragment>
-            <div className="absolute bottom-10 w-6/12 right-0 shadow-xl rounded-lg bg-white">
+            <div className="absolute bottom-10 w-6/12 right-0 shadow-xl rounded-lg bg-gray-100">
                 <div className="h-9 border-b px-5 flex items-center justify-between">
                     <span className="text-lg">Realtime Data on placed offers</span>
                     <svg className="h-6 text-gray-700 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -27,7 +27,7 @@ const RealtimeFloatingStats = (props: Props) => {
                         <span>{time}</span>
                     </div>
                 </div>
-                <div className="w-full">
+                <div className="w-full max-h-96 overflow-y-scroll">
                     <div className="py-2 min-w-full align-middle inline-block sm:px-2">
                         <div className="shadow align-middle border-b min-w-full border-gray-200 sm:rounded-lg">
                             <table className="min-w-full divide-y divide-gray-200" >
@@ -65,9 +65,7 @@ const RealtimeFloatingStats = (props: Props) => {
                                             <span>GHC</span>
                                         </td>
                                         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <svg onClick={() => { }} className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                            </svg>
+                                            <span></span>
                                         </td>
                                     </tr>
                                 </tbody>
