567a695a480ccf71a80c64d3168ea78cb231457f
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 00:27:23 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeFloatingStats.tsx
+++ src/components/RealtimeFloatingStats.tsx
@@ -40,6 +40,30 @@ const RealtimeFloatingStats = (props: Props) => {
                                         <th>Reinsurer</th>
                                     </tr>
                                 </thead>
+                                <tbody>
+                                    <tr className="bg-white">
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                            National Insurance Company Limited
+                                                  </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            <span>GHC</span>
+                                            <span>12,000</span>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            <span>GHC</span>
+                                            <span>12,000</span>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            <span>GHC</span>
+                                            <span>12,000</span>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <svg onClick={() => {}} className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                                            </svg>
+                                        </td>
+                                    </tr>
+                                </tbody>
                             </table>
                         </div>
                     </div>
