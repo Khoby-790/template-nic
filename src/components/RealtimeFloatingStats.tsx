b47a305aae0aa21a77fe4dfece6edbed5304a4c7
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 18:33:39 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeFloatingStats.tsx
+++ src/components/RealtimeFloatingStats.tsx
@@ -19,8 +19,8 @@ const RealtimeFloatingStats = (props: Props) => {
 
     if (!showDetails) {
         return (
-            <div onClick={() => setShowDetails(true)} className="absolute group bottom-10 w-auto  right-6 shadow-xl rounded-lg p-2 bg-yellow-300">
-                <svg className="h-6 animate-ping group-hover:animate-none" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div onClick={() => setShowDetails(true)} className="absolute animate-bounce group bottom-10 w-auto  right-6 shadow-xl rounded-lg p-2 bg-yellow-300">
+                <svg className="h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
                 </svg>
