c6c7130dd1b24376f93db6d636fc57b04435a459
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 09:53:35 +0000

    --- Auto-Git Commit ---

--- src/pages/ClaimsExpansion/components/ReinsurerPill.tsx
+++ src/pages/ClaimsExpansion/components/ReinsurerPill.tsx
@@ -1,17 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import { Modal } from '../../../components'
+import ReinsurersList from '../tabs/ReinsurersList';
 import DisplayBarChart from './DisplayBarChart'
 
 interface Props {
     name?: string
 }
 
+const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
+const active = "border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
+
+type Tabs = "stats" | "reinsurers"
+
 const ReinsurerPill = ({ name }: Props) => {
-    const [showGraph, setShowGraph] = useState<boolean>(false)
+    const [showGraph, setShowGraph] = useState<boolean>(false);
+    const [tabs, setTabs] = useState<Tabs>("stats");
     return (
         <>
-            <div className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+            <div className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-start space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                 <div className="flex-shrink-0">
                     <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=ah3lxr8uqw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                 </div>
@@ -23,32 +30,54 @@ const ReinsurerPill = ({ name }: Props) => {
                                 <p className="text-sm font-medium text-gray-900">
                                     {name || "Leslie Alexander"}
                                 </p>
-                                <p className="text-sm flex items-center text-gray-500 truncate">
-                                    <svg className="h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <p className="text-sm flex items-start text-gray-500 truncate">
+                                    <svg className="h-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                                     </svg>
-                                    <span>East Legon</span>
+                                    <div className="flex flex-col">
+                                        <span>Address </span>
+                                        <span>Contact Details</span>
+                                    </div>
                                 </p>
                             </a>
-                            <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
-                                <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                </svg>
+                            <div className="flex">
+                                <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
+                                    <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                </div>
+                                <div onClick={() => setShowGraph(true)} className="flex w-20 cursor-pointer justify-center text-gray-500 items-center rounded-xl hover:bg-indigo-100 p-2 my-3">
+                                    <svg className="h-6 text-sm text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                    </svg>
+                                </div>
                             </div>
+
+
                         </div>
-                        <div className="px-5 grid grid-cols-3 gap-x-5">
-                            <div className="flex flex-col">
-                                <span className="font-thin">Total</span>
-                                <span className="font-semibold text-xl">12,022,152</span>
+                        <div>
+                            <div className="py-3 flex justify-end">
+                                <label className="mr-3" htmlFor="">Filter by currency</label>
+                                <select name="" id="">
+                                    <option value="">GHC</option>
+                                    <option value="">EUR</option>
+                                    <option value="">USD</option>
+                                </select>
                             </div>
-                            <div className="flex flex-col">
-                                <span className="font-thin">Paid</span>
-                                <span className="font-semibold text-xl">12,022,152</span>
-                            </div>
-                            <div className="flex flex-col">
-                                <span className="font-thin">Outstanding</span>
-                                <span className="font-semibold text-xl">12,022,152</span>
+                            <div className="px-5 grid grid-cols-3 gap-x-5">
+                                <div className="flex flex-col">
+                                    <span className="font-thin">Total</span>
+                                    <span className="font-semibold text-xl">GHC 12,022,152</span>
+                                </div>
+                                <div className="flex flex-col">
+                                    <span className="font-thin">Paid</span>
+                                    <span className="font-semibold text-xl"> GHC 12,022,152</span>
+                                </div>
+                                <div className="flex flex-col">
+                                    <span className="font-thin">Outstanding</span>
+                                    <span className="font-semibold text-xl">GHC 12,022,152</span>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -56,7 +85,33 @@ const ReinsurerPill = ({ name }: Props) => {
             </div>
 
             <Modal size={80} show={showGraph} setShow={setShowGraph}>
-                <DisplayBarChart />
+                <Fragment>
+                    <div className="px-5 mt-4">
+                        <div className="sm:hidden">
+                            <label htmlFor="tabs" className="sr-only">Select a tab</label>
+                            <select id="tabs" name="tabs" className="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                                <option>My Account</option>
+                                <option>Company</option>
+                                <option selected>Team Members</option>
+                                <option>Billing</option>
+                            </select>
+                        </div>
+                        <div className="hidden sm:block">
+                            <div className="border-b border-gray-200">
+                                <nav className="-mb-px flex" aria-label="Tabs">
+                                    <a href="#" onClick={() => setTabs("stats")} className={tabs === "stats" ? active : inActive}>
+                                        NIC levy summary
+                                    </a>
+                                    <a href="#" onClick={() => setTabs("reinsurers")} className={tabs === "reinsurers" ? active : inActive}>
+                                        Reinsurers
+                                    </a>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                    {tabs === "stats" && <DisplayBarChart />}
+                    {tabs === "reinsurers" && <ReinsurersList />}
+                </Fragment>
             </Modal>
         </>
     )
--- src/pages/ClaimsExpansion/tabs/ReinsurersList.tsx
+++ src/pages/ClaimsExpansion/tabs/ReinsurersList.tsx
@@ -1,27 +1,26 @@
-import React, { useRef, useState } from 'react'
-import { Modal, Transition } from '../../../components'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState, useRef } from 'react'
+import { Transition } from '../../../components';
 import { useOutsideClick } from '../../../hooks';
-import DisplayBarChart from '../components/DisplayBarChart'
 
 interface Props {
 
 }
 
-const FromEachReinsurerTab = (props: Props) => {
+const ReinsurersList = (props: Props) => {
     const saveOptionsREf = useRef(null);
-    const [showGraph, setShowGraph] = useState<boolean>(false)
     const [showSaveOptions, setShowSaveOptions] = useState<boolean>(false);
-    useOutsideClick(saveOptionsREf, () => setShowSaveOptions(false))
+    useOutsideClick(saveOptionsREf, () => setShowSaveOptions(false));
     return (
-        <div className="">
+        <div>
             <div className="h-auto px-4 py-3 flex justify-between">
                 <div className="flex flex-col">
                     <span className="text-xl font-bold">Summary</span>
-                    {/* <span className="font-light">Visal Reinsurance Brokers</span> */}
+                    <span className="font-light">Visal Reinsurance Brokers</span>
                     <span className="font-light">Total NIC Levies for 2021</span>
                 </div>
-                <div className="flex">
-                    <div className="flex border  mr-2 p-2 mb-1 rounded items-baseline">
+                <div>
+                    <div className="flex border p-2 mb-1 rounded items-baseline">
                         <label className="mr-3" htmlFor="">Filter by currency</label>
                         <select name="" id="">
                             <option value="">GHC</option>
@@ -93,7 +92,6 @@ const FromEachReinsurerTab = (props: Props) => {
 
                 </div>
             </div>
-
             <div className="py-4">
                 <div className="flex flex-col">
                     <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -114,17 +112,14 @@ const FromEachReinsurerTab = (props: Props) => {
                                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Total
                                         </th>
-                                            <th scope="col" className="relative px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Actions
-                                        </th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         {/* <!-- Odd row --> */}
                                         <tr className="bg-white">
                                             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                                National Insurance Company Limited
-                                                  </td>
+                                                Aveni Reinsurance Company Limited
+                                        </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                                 <span>GHC</span>
                                                 <span>12,000</span>
@@ -137,17 +132,13 @@ const FromEachReinsurerTab = (props: Props) => {
                                                 <span>GHC</span>
                                                 <span>12,000</span>
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <svg onClick={() => setShowGraph(true)} className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                                </svg>
-                                            </td>
+
                                         </tr>
 
                                         {/* <!-- Even row --> */}
                                         <tr className="bg-gray-50">
                                             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                                Royal Insurance Company Limited
+                                                Ghana Reinsurance Company Limited
                                         </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                                 <span>GHC</span>
@@ -161,15 +152,11 @@ const FromEachReinsurerTab = (props: Props) => {
                                                 <span>GHC</span>
                                                 <span>12,000</span>
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <svg onClick={() => setShowGraph(true)} className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                                </svg>
-                                            </td>
+
                                         </tr>
                                         <tr className="bg-white">
                                             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                                Vanguard Assurance Company Limited
+                                                GN Reinsurance Company Limited
                                         </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                                 <span>GHC</span>
@@ -183,15 +170,11 @@ const FromEachReinsurerTab = (props: Props) => {
                                                 <span>GHC</span>
                                                 <span>12,000</span>
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <svg onClick={() => setShowGraph(true)} className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                                </svg>
-                                            </td>
+
                                         </tr>
                                         <tr className="bg-gray-50">
                                             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                                Enterprise Insurance Company Limited
+                                                Mainstream Reinsurance Company Limited
                                         </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                                 <span>GHC</span>
@@ -205,13 +188,8 @@ const FromEachReinsurerTab = (props: Props) => {
                                                 <span>GHC</span>
                                                 <span>12,000</span>
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <svg onClick={() => setShowGraph(true)} className="text-indigo-600 h-6 cursor-pointer hover:text-indigo-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                                </svg>
-                                            </td>
-                                        </tr>
 
+                                        </tr>
                                         {/* <!-- More items... --> */}
                                     </tbody>
                                 </table>
@@ -219,60 +197,55 @@ const FromEachReinsurerTab = (props: Props) => {
                         </div>
                     </div>
                 </div>
-                {/* <!-- This example requires Tailwind CSS v2.0+ --> */}
-                <nav className="border-t mt-3 border-gray-200 px-4 flex items-center justify-between sm:px-0">
-                    <div className="-mt-px w-0 flex-1 flex">
-                        <a href="#" className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
-                            {/* <!-- Heroicon name: solid/arrow-narrow-left --> */}
-                            <svg className="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
-                            </svg>
+            </div>
+            <nav className="border-t mt-3 mb-5 mx-5 border-gray-200 px-4 flex items-center justify-between sm:px-0">
+                <div className="-mt-px w-0 flex-1 flex">
+                    <a href="#" className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
+                        {/* <!-- Heroicon name: solid/arrow-narrow-left --> */}
+                        <svg className="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                        </svg>
       Previous
     </a>
-                    </div>
-                    <div className="hidden md:-mt-px md:flex">
-                        <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
-                            1
+                </div>
+                <div className="hidden md:-mt-px md:flex">
+                    <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
+                        1
     </a>
-                        {/* <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" --> */}
-                        <a href="#" className="border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium" aria-current="page">
-                            2
+                    {/* <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" --> */}
+                    <a href="#" className="border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium" aria-current="page">
+                        2
     </a>
-                        <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
-                            3
+                    <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
+                        3
     </a>
-                        <span className="border-transparent text-gray-500 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
-                            ...
+                    <span className="border-transparent text-gray-500 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
+                        ...
     </span>
-                        <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
-                            8
+                    <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
+                        8
     </a>
-                        <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
-                            9
+                    <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
+                        9
     </a>
-                        <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
-                            10
+                    <a href="#" className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
+                        10
     </a>
-                    </div>
-                    <div className="-mt-px w-0 flex-1 flex justify-end">
-                        <a href="#" className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
-                            Next
+                </div>
+                <div className="-mt-px w-0 flex-1 flex justify-end">
+                    <a href="#" className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
+                        Next
       {/* <!-- Heroicon name: solid/arrow-narrow-right --> */}
-                            <svg className="ml-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                    </div>
-                </nav>
+                        <svg className="ml-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </div>
+            </nav>
 
 
-                <Modal size={80} show={showGraph} setShow={setShowGraph}>
-                    <DisplayBarChart />
-                </Modal>
-
-            </div>
         </div>
     )
 }
 
-export default FromEachReinsurerTab
+export default ReinsurersList
