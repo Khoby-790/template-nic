e2ebbf339f1ddd5fa997b0d259e50f78b6fad770
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 10:30:52 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeBreakDown.tsx
+++ src/components/RealtimeBreakDown.tsx
@@ -15,6 +15,11 @@ const RealtimeBreakDown = (props: Props) => {
     return (
         <Fragment>
             <div className="bg-gray-300 max-h-64 h-full px-4 py-5 overflow-y-scroll">
+                <div className="flex justify-between">
+                    <svg className="text-gray-600 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                    </svg>
+                </div>
                 <div>
                     {colors.map((color, id) => (
                         <div className="my-2 flex justify-between" key={id}>
