cd7f9829acb9190967db39adc91050dbf486c234
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 10:29:46 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeBreakDown.tsx
+++ src/components/RealtimeBreakDown.tsx
@@ -4,6 +4,10 @@ interface Props {
 
 }
 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 const colors = ["green", "red", "yellow", "indigo"]
 const companies = ["VRB", "KRB", "ARB", "VRBb"]
 
@@ -13,8 +17,9 @@ const RealtimeBreakDown = (props: Props) => {
             <div className="bg-gray-300 max-h-64 h-full px-4 py-5 overflow-y-scroll">
                 <div>
                     {colors.map((color, id) => (
-                        <div className="my-2" key={id}>
+                        <div className="my-2 flex justify-between" key={id}>
                             <span className={`bg-${color}-300 px-2 rounded text-${color}-700`}>{companies[id]}</span>
+                            <span>{getRandomArbitrary(6, 25).toFixed()}</span>
                         </div>
                     ))}
                 </div>
