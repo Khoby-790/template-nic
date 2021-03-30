0e65dad9402bff36e5ca3c62d77a09347e5803e7
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 00:06:37 +0000

    --- Auto-Git Commit ---

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -92,7 +92,7 @@ const Sidebar = (props: Props) => {
                                     Premium
                 </Link>
 
-                                <Link to="/"
+                                <Link to="/claims"
                                     className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
                                 >
                                     {/* <!-- Heroicon name: outline/calendar --> */}
@@ -114,7 +114,7 @@ const Sidebar = (props: Props) => {
                                     Claims
                 </Link>
 
-                                <Link to="/"
+                                <Link to="/_offers"
                                     className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
                                 >
                                     {/* <!-- Heroicon name: outline/inbox --> */}
