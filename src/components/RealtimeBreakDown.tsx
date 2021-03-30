1c030d6be8850f7bf81805b8385edba1ff5af0be
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 10:25:16 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeBreakDown.tsx
+++ src/components/RealtimeBreakDown.tsx
@@ -8,7 +8,10 @@ const RealtimeBreakDown = (props: Props) => {
     return (
         <Fragment>
             <div className="bg-gray-300 max-h-64 h-full px-4 py-5 overflow-y-scroll">
-                Hello
+                <div>
+                    
+                </div>
+                <div></div>
             </div>
         </Fragment>
     )
