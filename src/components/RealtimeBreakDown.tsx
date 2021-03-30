d18fbb572c81a84f94b8b77c61917676838e190e
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 10:27:35 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeBreakDown.tsx
+++ src/components/RealtimeBreakDown.tsx
@@ -5,6 +5,7 @@ interface Props {
 }
 
 const colors = ["green", "red", "yellow", "indigo"]
+const companies = ["VRB", "KRB", "ARB", "VRBb"]
 
 const RealtimeBreakDown = (props: Props) => {
     return (
@@ -12,7 +13,9 @@ const RealtimeBreakDown = (props: Props) => {
             <div className="bg-gray-300 max-h-64 h-full px-4 py-5 overflow-y-scroll">
                 <div>
                     {colors.map((color, id) => (
-                        <div key={id}></div>
+                        <div key={id}>
+                            <span className={`bg-${color}-300 px-2 rounded text-${color}-700`}>{companies[id]}</span>
+                        </div>
                     ))}
                 </div>
                 <div></div>
