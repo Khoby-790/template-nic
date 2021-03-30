854f02abfda00a8a5352bf4911a4e8d4d4fc37d5
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 16:12:53 +0000

    --- Auto-Git Commit ---

--- src/pages/NewDashboard/sections/OffersPlaced.jsx
+++ src/pages/NewDashboard/sections/OffersPlaced.jsx
@@ -24,26 +24,38 @@ const OffersPlaced = () => {
       <Ringcomponent data={myData} content={myContent} />
       <Ringcomponent data={myData} content={content2} />
       <div className="mx-3 py-4 px-5 flex-col flex-1 flex justify-center items-center">
-        <div className="flex h-7 justify-end w-full">
-          <Link
-            to="/_offers"
-            className=" flex justify-center h-full items-center px-4 cursor-pointer hover:bg-gray-400 rounded-full"
-          >
-            <svg
-              className="h-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+        <div className="flex h-auto justify-end w-full">
+          <div className="flex">
+            <div className="flex border  mr-2 p-2 mb-1 rounded items-baseline">
+              <label className="mr-3" htmlFor="">
+                currency
+              </label>
+              <select name="" id="">
+                <option value="">GHC</option>
+                <option value="">GHC</option>
+                <option value="">GHC</option>
+              </select>
+            </div>
+            <Link
+              to="/_offers"
+              className=" flex justify-center h-full items-center px-4 cursor-pointer hover:bg-gray-400 rounded-full"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-              />
-            </svg>
-          </Link>
+              <svg
+                className="h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                />
+              </svg>
+            </Link>
+          </div>
         </div>
         <div className="flex mb-4 flex-col">
           <span className="text-2xl font-semibold">78,564</span>
