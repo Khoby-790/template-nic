9b2e3ca74e7792538b1a2aab5cb37496568111c5
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 10:48:20 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeFloatingStats.tsx
+++ src/components/RealtimeFloatingStats.tsx
@@ -19,8 +19,11 @@ const RealtimeFloatingStats = (props: Props) => {
 
     if (!showDetails) {
         return (
-            <div className="absolute bottom-10 w-auto right-6 shadow-xl rounded-lg p-4 bg-indigo-600">
-                Hello
+            <div className="absolute bottom-10 w-auto right-6 shadow-xl rounded-lg p-2 bg-indigo-600">
+                <svg className="h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
+                </svg>
             </div>
         )
     }
