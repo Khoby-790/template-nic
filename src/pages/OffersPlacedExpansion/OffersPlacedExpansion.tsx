79a69be3289dd08042821de6583458383d76e1f5
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 00:09:53 +0000

    --- Auto-Git Commit ---

--- src/pages/OffersPlacedExpansion/OffersPlacedExpansion.tsx
+++ src/pages/OffersPlacedExpansion/OffersPlacedExpansion.tsx
@@ -177,12 +177,9 @@ const OffersPlacedExpansion = (props: Props) => {
                                         For All Brokers
                                 </a>
                                     <a href="#" onClick={() => setTab("for_each_broker")} className={tab === "for_each_broker" ? active : inActive}>
-                                        From Each Broker
+                                        From Each Reinsured
                                 </a>
-                                    {/* <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" --> */}
-                                    <a href="#" onClick={() => setTab("for_all_reinsurers")} className={tab === "for_all_reinsurers" ? active : inActive} aria-current="page">
-                                        For Each Reinsured
-                                </a>
+
                                     <a href="#" onClick={() => setTab("for_each_reinsurer")} className={tab === "for_each_reinsurer" ? active : inActive}>
                                         From Each Reinsurer
                                 </a>
