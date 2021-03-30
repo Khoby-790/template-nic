9dace66fde8be0ddb43085274d6cd57dc4b2d2d3
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 10:20:54 +0000

    --- Auto-Git Commit ---

---
+++ src/components/RealtimeBreakDown.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+interface Props {
+
+}
+
+const RealtimeBreakDown = (props: Props) => {
+    return (
+        <Fragment>
+            <div className="bg-gray-300 max-h-64 h-full overflow-y-scroll">
+                Hello
+            </div>
+        </Fragment>
+    )
+}
+
+export default RealtimeBreakDown
