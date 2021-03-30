494a2b2b22dc8ca713d3676477e4db5ba79bfdd9
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 18:47:53 +0000

    --- Auto-Git Commit ---

--- src/pages/NewDashboard/sections/WelcomeSection.tsx
+++ src/pages/NewDashboard/sections/WelcomeSection.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-
+import avatar from '../../../assets/23511370.jpg'
 interface Props {
 
 }
@@ -15,7 +15,7 @@ const WelcomeSection = (props: Props) => {
                         <div className="sm:flex sm:items-center sm:justify-between">
                             <div className="sm:flex sm:space-x-5">
                                 <div className="flex-shrink-0">
-                                    <img className="mx-auto h-20 w-20 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="" />
+                                    <img className="mx-auto h-20 w-20 rounded-full" src={avatar} alt="" />
                                 </div>
                                 <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
                                     <p className="text-sm font-medium text-gray-100">Welcome back,</p>
