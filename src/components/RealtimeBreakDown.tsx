7e752f9294279ffee286a5628c78738ec0ca0254
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 10:34:10 +0000

    --- Auto-Git Commit ---

--- src/components/RealtimeBreakDown.tsx
+++ src/components/RealtimeBreakDown.tsx
@@ -23,7 +23,7 @@ const RealtimeBreakDown = (props: Props) => {
                         </svg>
                         View more
                     </div>
-                    <svg className="text-gray-600 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg className="text-gray-600 cursor-pointer h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
                     </svg>
                 </div>
