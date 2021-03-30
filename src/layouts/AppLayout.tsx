7c884012e026e9302980417dbb5160ec3da32f2f
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 00:15:21 +0000

    --- Auto-Git Commit ---

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -25,8 +25,11 @@ const AppLayout = ({ history, match }: Props) => {
                 </div>
             </div>
             <div className="absolute bottom-10 w-9/12 right-0 shadow-xl rounded-lg px-5 bg-white">
-                <div className="">
+                <div className="h-9 flex items-center justify-between">
                     <span className="text-lg">Realtime Data on placed offers</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
+                    </svg>
                 </div>
 
             </div>
