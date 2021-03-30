c64c392d098c88e5a74a63164241a681398893b1
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 10:06:42 +0000

    --- Auto-Git Commit ---

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -45,30 +45,18 @@ const Sidebar = (props: Props) => {
                                             d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                                         />
                                     </svg>
-                  Dashboard
-                </Link>
+                                    Dashboard
+                                </Link>
 
                                 <Link to="/nic"
                                     className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
                                 >
                                     {/* <!-- Heroicon name: outline/users --> */}
-                                    <svg
-                                        className="text-gray-400 group-hover:text-gray-300 mr-3 h-6 w-6"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        aria-hidden="true"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-                                        />
+                                    <svg className="text-gray-400 group-hover:text-gray-300 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
                                     NIC Levy
-                </Link>
+                                </Link>
 
                                 <Link to="/premium"
                                     className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
@@ -90,7 +78,7 @@ const Sidebar = (props: Props) => {
                                         />
                                     </svg>
                                     Premium
-                </Link>
+                                </Link>
 
                                 <Link to="/claims"
                                     className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
@@ -112,7 +100,7 @@ const Sidebar = (props: Props) => {
                                         />
                                     </svg>
                                     Claims
-                </Link>
+                                </Link>
 
                                 <Link to="/_offers"
                                     className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
@@ -134,7 +122,7 @@ const Sidebar = (props: Props) => {
                                         />
                                     </svg>
                   Offers placed
-                </Link>
+                                </Link>
 
                                 <Link to="/reports"
                                     className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
@@ -156,7 +144,7 @@ const Sidebar = (props: Props) => {
                                         />
                                     </svg>
                   Reports
-                </Link>
+                                </Link>
                             </nav>
                         </div>
                         <div className="flex-shrink-0 flex bg-gray-700 p-4">
