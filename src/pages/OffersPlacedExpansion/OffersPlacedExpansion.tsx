3ef200a3255deb65c960c5a5a263169288b856ea
Khoby-790 <ebaidoo79@gmail.com>
2021-03-30 00:10:59 +0000

    --- Auto-Git Commit ---

--- src/pages/OffersPlacedExpansion/OffersPlacedExpansion.tsx
+++ src/pages/OffersPlacedExpansion/OffersPlacedExpansion.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Fragment, useState } from 'react'
 import { PageBreadCrum } from '../../components';
 import SubGraphsBarchart from '../Dashboard/components/SubGraphsBarchart';
@@ -86,8 +87,8 @@ const data = [
 ];
 
 
-const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
-const active = "border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
+const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm";
+const active = "border-indigo-500 text-indigo-600 flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm";
 
 type Tabs = "for_each_reinsurer" | "for_each_broker" | "for_all_brokers" | "for_all_reinsurers"
 
