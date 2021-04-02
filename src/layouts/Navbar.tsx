1f3a00c3ab0bf97797c74e7ec35572ba021404ee
Khoby-790 <ebaidoo79@gmail.com>
2021-04-02 00:34:43 +0000

    --- Auto-Git Commit ---

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = (props: Props) => {
                 {/* <!-- Menu button area --> */}
                 <div className="absolute inset-y-0 right-0 pr-4 flex items-center sm:pr-6 md:hidden">
                     {/* <!-- Mobile menu button --> */}
-                    <button type="button" className="-mr-2 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600" aria-expanded="false">
+                    <button onClick={() => setShowSideBar(true)} type="button" className="-mr-2 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         {/* <!-- Heroicon name: outline/menu --> */}
                         <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -162,7 +162,7 @@ const Navbar = (props: Props) => {
                                     <a href="#">
                                         <img className="block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=500" alt="Workflow" />
                                     </a>
-                                    <button type="button" className="-mr-2 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600" aria-expanded="false">
+                                    <button onClick={() => setShowSideBar(false)} type="button" className="-mr-2 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600" aria-expanded="false">
                                         <span className="sr-only">Open main menu</span>
                                         {/* <!-- Heroicon name: outline/x --> */}
                                         <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
