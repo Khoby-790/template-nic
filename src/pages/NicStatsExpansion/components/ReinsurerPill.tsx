1b0478dce892733eb9acc7a322d0c859057b9ff0
Khoby-790 <ebaidoo79@gmail.com>
2021-04-02 01:19:37 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/components/ReinsurerPill.tsx
+++ src/pages/NicStatsExpansion/components/ReinsurerPill.tsx
@@ -22,7 +22,7 @@ const ReinsurerPill = ({ name }: Props) => {
                 <div className="flex-shrink-0">
                     <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=ah3lxr8uqw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                 </div>
-                <div className="flex-1 min-w-0">
+                <div className="flex-1 mt-3 min-w-0">
                     <div className="flex flex-col lg:flex-row">
                         <div className="">
                             <a href="#" className="focus:outline-none">
