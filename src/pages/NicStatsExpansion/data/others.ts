1704c2b3e619cd2af7b37393ef672bb45b864d9c
Khoby-790 <ebaidoo79@gmail.com>
2021-04-09 13:03:25 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/data/others.ts
+++ src/pages/NicStatsExpansion/data/others.ts
@@ -1,25 +1,25 @@
 export const brokers: Broker[] = [
   {
     id: 1,
-    name: "Schinner Inc",
+    name: "Visal Reinsurance Brokers",
     address: "81 Huxley Way",
     contact: "+591 (191) 964-1534",
   },
   {
     id: 2,
-    name: "Bosco Inc",
+    name: "iRisk Reinsurance Brokers",
     address: "2259 Kipling Plaza",
     contact: "+86 (766) 125-2181",
   },
   {
     id: 3,
-    name: "Jast, Schowalter and Considine",
+    name: "Kek Reinsurance Brokers",
     address: "4862 Columbus Crossing",
     contact: "+7 (753) 334-9191",
   },
   {
     id: 4,
-    name: "Hermann, Pfeffer and Emmerich",
+    name: "Afro-asian Reinsurance Brokers",
     address: "03 Lukken Drive",
     contact: "+86 (373) 905-3576",
   },
