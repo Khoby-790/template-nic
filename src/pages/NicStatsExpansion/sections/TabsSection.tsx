e21cf970ad0bb8e64c6aaec7de323ee71e9744ea
Khoby-790 <ebaidoo79@gmail.com>
2021-04-09 12:37:12 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/sections/TabsSection.tsx
+++ src/pages/NicStatsExpansion/sections/TabsSection.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Fragment, useState } from 'react'
 
 interface Props {
-    
+
 }
 
+
+const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm";
+const active = "border-indigo-500 text-indigo-600 flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm";
+
 const TabsSection = (props: Props) => {
+    const [tab, setTab] = useState<string>("for_each_reinsurer")
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <div>
+                <div className="sm:hidden my-3">
+                    <label htmlFor="tabs" className="sr-only">Select a tab</label>
+                    <select onChange={e => setTab(e.target.value)} id="tabs" name="tabs" className="block w-full p-2 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                        <option value="for_all_brokers">For All Brokers</option>
+                        <option value="for_each_reinsurer">From Each Reinsured</option>
+                        <option value="for_each_broker">From Each Reinsurer</option>
+                    </select>
+                </div>
+                <div className="hidden sm:block">
+                    <div className="border-b border-gray-200">
+                        <nav className="-mb-px flex" aria-label="Tabs">
+                            <a href="#" onClick={() => setTab("for_all_brokers")} className={tab === "for_all_brokers" ? active : inActive}>
+                                For All Brokers
+                                </a>
+                            <a href="#" onClick={() => setTab("for_each_reinsurer")} className={tab === "for_each_reinsurer" ? active : inActive}>
+                                From Each Reinsured
+                                </a>
+
+                            <a href="#" onClick={() => setTab("for_each_broker")} className={tab === "for_each_broker" ? active : inActive}>
+                                From Each Reinsurer
+                                </a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     )
 }
 
