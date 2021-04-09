7e4ca49619dded0c1eed5480beeec8cb9aa143fa
Khoby-790 <ebaidoo79@gmail.com>
2021-04-09 12:36:07 +0000

    --- Auto-Git Commit ---

---
+++ src/pages/NicStatsExpansion/sections/TabsSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const TabsSection = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TabsSection
