4113e8240307d6bd0ea663f7041f17868932b2db
Khoby-790 <ebaidoo79@gmail.com>
2021-04-09 13:07:47 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/tabs/ForEachReinsurerTab.tsx
+++ src/pages/NicStatsExpansion/tabs/ForEachReinsurerTab.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import ReinsurerPill from '../components/ReinsurerPill'
+import { insurers } from '../data/others'
 
 interface Props {
 
@@ -10,9 +11,10 @@ const ForEachReinsurerTab = (props: Props) => {
     return (
         <div className="py-4">
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                <ReinsurerPill name="Visal Insurance Company" />
+                {insurers.map((insurer, insurerId)=><ReinsurerPill broker={insurer} key={insurerId} />)}
+                {/* <ReinsurerPill name="Visal Insurance Company" />
                 <ReinsurerPill name="Kek Insurance Company" />
-                <ReinsurerPill name="iRisk Insurance Company" />
+                <ReinsurerPill name="iRisk Insurance Company" /> */}
             </div>
 
         </div>
