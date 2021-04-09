ff30133bfdbafbde5c76b5f5af02a13655fd05e4
Khoby-790 <ebaidoo79@gmail.com>
2021-04-09 13:04:31 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/components/ReinsurerPill.tsx
+++ src/pages/NicStatsExpansion/components/ReinsurerPill.tsx
@@ -5,7 +5,8 @@ import ReinsurersList from '../tabs/ReinsurersList';
 import DisplayBarChart from './DisplayBarChart'
 
 interface Props {
-    name?: string
+    name?: string;
+    broker: Broker;
 }
 
 const inActive = "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm";
