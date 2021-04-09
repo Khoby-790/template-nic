714c812e029ca1f505b62790538cf4aaf68573e2
Khoby-790 <ebaidoo79@gmail.com>
2021-04-09 12:52:31 +0000

    --- Auto-Git Commit ---

--- src/pages/NicStatsExpansion/NicStatsExpansion.tsx
+++ src/pages/NicStatsExpansion/NicStatsExpansion.tsx
@@ -1,44 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 import { PageBreadCrum } from '../../components';
-import SubGraphsBarchart from '../Dashboard/components/SubGraphsBarchart';
 import GraphSection from './sections/GraphSection';
 import StatsSection from './sections/StatsSection';
 import TabsSection from './sections/TabsSection';
-import ForAllBrokers from './tabs/ForAllBrokers';
-import FromEachReinsuredTab from './tabs/FromEachReinsuredTab';
-import FromEachReinsurerTab from './tabs/FromEachReinsurerTab';
+
 
 interface Props {
 
 }
 
 
-
-
-
-
-
-
-
 const NicStatsExpansion = (props: Props) => {
-    const [tab, setTab] = useState<string>("for_each_reinsurer")
     return (
         <Fragment>
             <div className="px-3 py-4">
                 <div>
                     <PageBreadCrum page="NIC Levy Overview" />
-                    {/* <div /> */}
                     <GraphSection />
                     <StatsSection />
                     <TabsSection />
-
-                    <Fragment>
-                        {tab === "for_all_brokers" && <ForAllBrokers />}
-                        {tab === "for_each_broker" && <FromEachReinsurerTab />}
-                        {tab === "for_each_reinsurer" && <FromEachReinsuredTab />}
-                    </Fragment>
-
                 </div>
             </div>
         </Fragment>
